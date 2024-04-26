d90632b0b1f9ba6654a3fd2e9891a57eeff9bffa
Siddhartha Basu <biosidd@gmail.com>
2024-04-26 15:15:15 -0500

    feat(service.go): add port parameter to serviceSpecArgs and servicePortSpec functions to allow dynamic port assignment
    The strconv import was removed as it was no longer needed. The port is
    now directly taken from the configuration file, which makes the
    application more flexible and easier to configure. The serviceSpecArgs
    and servicePortSpec functions now take an additional port parameter,
    allowing the port to be dynamically assigned based on the configuration.

--- backend_application/service.go
+++ backend_application/service.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/dictybase-docker/cluster-ops/k8s"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -11,29 +9,33 @@ import (
 func serviceSpecs(args *specProperties) *corev1.ServiceArgs {
 	return &corev1.ServiceArgs{
 		Metadata: k8s.Metadata(args.cfg, args.serviceName),
-		Spec:     serviceSpecArgs(args.deploymentName, args.serviceName),
+		Spec: serviceSpecArgs(
+			args.deploymentName,
+			args.serviceName,
+			args.cfg.GetInt("port"),
+		),
 	}
 }
 
 func serviceSpecArgs(
 	deploymentName, serviceName string,
+	port int,
 ) *corev1.ServiceSpecArgs {
 	return &corev1.ServiceSpecArgs{
 		Selector: pulumi.StringMap{
 			"app": pulumi.String(deploymentName),
 		},
-		Ports: servicePortSpec(serviceName),
+		Ports: servicePortSpec(serviceName, port),
 		Type:  pulumi.String("NodePort"),
 	}
 }
 
-func servicePortSpec(serviceName string) corev1.ServicePortArray {
-	targetPort, _ := strconv.Atoi(serviceName)
+func servicePortSpec(serviceName string, port int) corev1.ServicePortArray {
 	return corev1.ServicePortArray{
 		&corev1.ServicePortArgs{
 			Name:       pulumi.String(serviceName),
-			Port:       pulumi.Int(80),
-			TargetPort: pulumi.Int(targetPort),
+			Port:       pulumi.Int(port),
+			TargetPort: pulumi.String(serviceName),
 		},
 	}
 
