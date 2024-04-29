6828d3ab40dafc77944f4b3153bf5a3844888829
Siddhartha Basu <biosidd@gmail.com>
2024-04-28 21:15:16 -0500

    feat(main.go): add support for multiple apps and namespaces in the configuration
    The changes allow the application to handle multiple apps and namespaces
    from the configuration. This is achieved by creating a new struct
    'appProperties' to hold the properties of each app and adding a loop to
    iterate over all the apps in the configuration. This makes the
    application more flexible and scalable as it can now handle multiple
    apps and namespaces.

--- backend_application/main.go
+++ backend_application/main.go
@@ -9,11 +9,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
+type appProperties struct {
+	Image string
+	Tag   string
+	Port  int
+}
+
 type specProperties struct {
 	deploymentName string
 	serviceName    string
+	appName        string
+	namespace      string
 	cfg            *config.Config
 	ctx            *pulumi.Context
+	app            *appProperties
 }
 
 func main() {
@@ -22,33 +31,55 @@ func main() {
 
 func execute(ctx *pulumi.Context) error {
 	cfg := config.New(ctx, "")
-	deploymentName := fmt.Sprintf("%s-api-server", cfg.Require("name"))
-	serviceName := fmt.Sprintf("%s-api", cfg.Require("name"))
-	deployment, err := appsv1.NewDeployment(
-		ctx, deploymentName, deploymentSpec(&specProperties{
-			cfg:            cfg,
-			ctx:            ctx,
-			deploymentName: deploymentName,
-			serviceName:    serviceName,
-		}))
-	if err != nil {
-		return fmt.Errorf("error in running deployment %s", err)
+	appNames := make([]string, 0)
+	if err := cfg.TryObject("apps", &appNames); err != nil {
+		return fmt.Errorf(
+			"apps attribute is required in the configuration %s",
+			err,
+		)
 	}
-
-	_, err = corev1.NewService(
-		ctx,
-		serviceName,
-		serviceSpecs(
-			&specProperties{
+	namespace, err := cfg.Try("namespace")
+	if err != nil {
+		return fmt.Errorf("attribute namespace is missing %s", err)
+	}
+	for _, key := range appNames {
+		app := &appProperties{}
+		if err := cfg.TryObject(key, app); err != nil {
+			return fmt.Errorf("app name %s is required %s", key, err)
+		}
+		deploymentName := fmt.Sprintf("%s-api-server", key)
+		serviceName := fmt.Sprintf("%s-api", key)
+		deployment, err := appsv1.NewDeployment(
+			ctx, deploymentName, deploymentSpec(&specProperties{
+				cfg:            cfg,
+				ctx:            ctx,
 				deploymentName: deploymentName,
 				serviceName:    serviceName,
-				cfg:            cfg,
-			},
-		),
-		pulumi.DependsOn([]pulumi.Resource{deployment}),
-	)
-	if err != nil {
-		return fmt.Errorf("error in running service %s", err)
+				app:            app,
+				appName:        key,
+				namespace:      namespace,
+			}))
+		if err != nil {
+			return fmt.Errorf("error in running deployment %s", err)
+		}
+		_, err = corev1.NewService(
+			ctx,
+			serviceName,
+			serviceSpecs(
+				&specProperties{
+					deploymentName: deploymentName,
+					serviceName:    serviceName,
+					app:            app,
+					cfg:            cfg,
+					appName:        key,
+					namespace:      namespace,
+				},
+			),
+			pulumi.DependsOn([]pulumi.Resource{deployment}),
+		)
+		if err != nil {
+			return fmt.Errorf("error in running service %s", err)
+		}
 	}
 
 	return nil
