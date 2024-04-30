b6b6243a6988a5148ff193d51eb770e4363b9051
Siddhartha Basu <biosidd@gmail.com>
2024-04-29 20:43:14 -0500

    refactor(service.go): replace local serviceSpecArgs function with k8s.ServiceSpecArgs
    The local serviceSpecArgs function was removed from both
    backend_application/service.go and frontend_application/service.go.
    Instead, the k8s.ServiceSpecArgs function from the cluster-ops/k8s
    library is used.

--- backend_application/service.go
+++ backend_application/service.go
@@ -3,39 +3,15 @@ package main
 import (
 	"github.com/dictybase-docker/cluster-ops/k8s"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func serviceSpecs(args *specProperties) *corev1.ServiceArgs {
 	return &corev1.ServiceArgs{
 		Metadata: k8s.Metadata(args.namespace, args.serviceName),
-		Spec: serviceSpecArgs(
+		Spec: k8s.ServiceSpecArgs(
 			args.deploymentName,
 			args.serviceName,
 			args.app.Port,
 		),
-	}
-}
-
-func serviceSpecArgs(
-	deploymentName, serviceName string,
-	port int,
-) *corev1.ServiceSpecArgs {
-	return &corev1.ServiceSpecArgs{
-		Selector: pulumi.StringMap{
-			"app": pulumi.String(deploymentName),
-		},
-		Ports: servicePortSpec(serviceName, port),
-		Type:  pulumi.String("NodePort"),
-	}
-}
-
-func servicePortSpec(serviceName string, port int) corev1.ServicePortArray {
-	return corev1.ServicePortArray{
-		&corev1.ServicePortArgs{
-			Name:       pulumi.String(serviceName),
-			Port:       pulumi.Int(port),
-			TargetPort: pulumi.String(serviceName),
-		},
 	}
 }
