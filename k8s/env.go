aae5a80413295ad6794b35de2d636fe53c5c715d
Siddhartha Basu <biosidd@gmail.com>
2024-05-06 13:05:00 -0500

    feat(k8s/env.go): add new file to handle Kubernetes environment variables
    This new file, env.go, is created to handle Kubernetes environment
    variables. It includes functions to create environment variables,
    container environment specifications, and secret key selectors. This
    change is done to improve the organization of the code and make it
    easier to manage Kubernetes environment variables.

---
+++ k8s/env.go
@@ -0,0 +1,29 @@
+package k8s
+
+import (
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func CreateEnvVar(name, key, secret string) corev1.EnvVarArgs {
+	return corev1.EnvVarArgs{
+		Name: pulumi.String(name),
+		ValueFrom: corev1.EnvVarSourceArgs{
+			SecretKeyRef: CreateSecretKeySelector(key, secret),
+		},
+	}
+}
+
+func ContainerEnvSpec(secret string) corev1.EnvVarArray {
+	return corev1.EnvVarArray{
+		CreateEnvVar("ARANGODB_PASSWORD", "arangodb.password", secret),
+		CreateEnvVar("ARANGODB_USER", "arangodb.user", secret),
+	}
+}
+
+func CreateSecretKeySelector(key, secret string) corev1.SecretKeySelectorArgs {
+	return corev1.SecretKeySelectorArgs{
+		Name: pulumi.StringPtr(secret),
+		Key:  pulumi.String(key),
+	}
+}
