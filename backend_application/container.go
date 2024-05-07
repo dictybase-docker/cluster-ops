a3c8b23b1e4fc88b489f870afa5ac6714ce0e840
Siddhartha Basu <biosidd@gmail.com>
2024-05-07 13:06:55 -0500

    feat(container.go): add ARANGODB_PASSWORD and ARANGODB_USER environment variables to container spec
    The addition of ARANGODB_PASSWORD and ARANGODB_USER environment
    variables to the container specification allows the application to
    connect to an ArangoDB database. This is necessary for the application
    to interact with the database and perform CRUD operations.

--- backend_application/container.go
+++ backend_application/container.go
@@ -12,10 +12,17 @@ func containerSpec(args *specProperties) corev1.ContainerArray {
 	return []corev1.ContainerInput{corev1.ContainerArgs{
 		Name:  k8s.Container(args.appName),
 		Image: k8s.Image(args.app.Image, args.app.Tag),
-		Env:   k8s.ContainerEnvSpec(args.cfg.Require("secret")),
+		Env:   containerEnvSpec(args.cfg.Require("secret")),
 		Ports: k8s.ContainerPortSpec(args.app.Port, args.serviceName),
 		Args:  containerArgs(args.cfg.Get("log-level"), args.app.Port),
 	}}
+}
+
+func containerEnvSpec(secret string) corev1.EnvVarArray {
+	return corev1.EnvVarArray{
+		k8s.CreateEnvVar("ARANGODB_PASSWORD", "arangodb.password", secret),
+		k8s.CreateEnvVar("ARANGODB_USER", "arangodb.user", secret),
+	}
 }
 
 func containerArgs(log string, port int) pulumi.StringArrayInput {
