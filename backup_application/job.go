8607d1b51f98f0e1ca1b14b05a7b7d66e269905e
Siddhartha Basu <biosidd@gmail.com>
2024-05-07 20:13:33 -0500

    refactor(job.go): use app.jobName instead of jobName for better context
    The jobName property is now accessed through the app object, providing
    better context and clarity. This change helps to avoid potential naming
    conflicts and makes the code more readable by clearly indicating where
    the jobName is coming from.

--- backup_application/job.go
+++ backup_application/job.go
@@ -9,7 +9,7 @@ import (
 
 func jobPodTemplate(args *specProperties) *corev1.PodTemplateSpecArgs {
 	return &corev1.PodTemplateSpecArgs{
-		Metadata: k8s.TemplateMetadata(args.jobName),
+		Metadata: k8s.TemplateMetadata(args.app.jobName),
 		Spec: &corev1.PodSpecArgs{
 			RestartPolicy: pulumi.String("Never"),
 			Containers:    containerSpec(args),
@@ -20,13 +20,13 @@ func jobPodTemplate(args *specProperties) *corev1.PodTemplateSpecArgs {
 func jobSpecArgs(args *specProperties) *batchv1.JobSpecArgs {
 	return &batchv1.JobSpecArgs{
 		Template: jobPodTemplate(args),
-		Selector: k8s.SpecLabelSelector(args.jobName),
+		Selector: k8s.SpecLabelSelector(args.app.jobName),
 	}
 }
 
 func createJobSpec(args *specProperties) *batchv1.JobArgs {
 	return &batchv1.JobArgs{
-		Metadata: k8s.Metadata(args.namespace, args.jobName),
+		Metadata: k8s.Metadata(args.namespace, args.app.jobName),
 		Spec:     jobSpecArgs(args),
 	}
 }
