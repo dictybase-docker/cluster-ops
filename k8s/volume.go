c4757dca9425c8246fd251ba4beefd05b13cf2a3
Siddhartha Basu <biosidd@gmail.com>
2024-05-09 17:22:47 -0500

    feat(volume.go): add CreateVolumeSpec and volumeItems functions to create volume specifications
    The CreateVolumeSpec function was added to create volume specifications
    with secretName, volumeName, and items as parameters. This function will
    be useful in creating volume specifications for Kubernetes deployments.
    The volumeItems function was also added to convert VolumeItemsProperties
    into KeyToPathArray. This function will be used by CreateVolumeSpec to
    convert the items parameter into the required format. These changes will
    make it easier to create and manage volume specifications in Kubernetes
    deployments.

--- k8s/volume.go
+++ k8s/volume.go
@@ -5,6 +5,11 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+type VolumeItemsProperties struct {
+	Key   string
+	Value string
+}
+
 func CreateContainerVolumeMount(
 	volume, mountPath string,
 ) corev1.VolumeMountArgs {
@@ -12,4 +17,33 @@ func CreateContainerVolumeMount(
 		Name:      pulumi.String(volume),
 		MountPath: pulumi.String(mountPath),
 	}
+}
+
+func CreateVolumeSpec(
+	secretName, volumeName string,
+	items []*VolumeItemsProperties,
+) corev1.VolumeArray {
+	return corev1.VolumeArray{
+		corev1.VolumeArgs{
+			Name: pulumi.String(volumeName),
+			Secret: corev1.SecretVolumeSourceArgs{
+				SecretName: pulumi.String(secretName),
+				Items:      volumeItems(items),
+			},
+		},
+	}
+}
+
+func volumeItems(items []*VolumeItemsProperties) corev1.KeyToPathArray {
+	keyPathArr := make([]corev1.KeyToPathInput, 0)
+	for _, itm := range items {
+		keyPathArr = append(
+			keyPathArr,
+			corev1.KeyToPathArgs{
+				Key:  pulumi.String(itm.Key),
+				Path: pulumi.String(itm.Value),
+			},
+		)
+	}
+	return keyPathArr
 }
