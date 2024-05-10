4f563c985e5dc47d06837018d150b974c18d815b
Siddhartha Basu <biosidd@gmail.com>
2024-05-10 08:54:17 -0500

    feat(job.go): add support for PostgreSQL backup jobs
    This change introduces two new functions,
    createPostgresPodTemplateSpecArgs and createPostgresCronJobSpec. These
    functions allow the creation of a Kubernetes Pod and CronJob
    specifically for backing up PostgreSQL databases. This is a necessary
    feature for users who want to automate their PostgreSQL database
    backups. The Pod created by createPostgresPodTemplateSpecArgs includes a
    container running a PostgreSQL backup process and a volume for storing
    the backup data. The CronJob created by createPostgresCronJobSpec runs
    this Pod on a schedule defined by the user.

--- backup_application/job.go
+++ backup_application/job.go
@@ -24,12 +24,47 @@ func jobPodTemplate(args *specProperties) *corev1.PodTemplateSpecArgs {
 	}
 }
 
+func createPostgresPodTemplateSpecArgs(
+	args *specProperties,
+) *corev1.PodTemplateSpecArgs {
+	return &corev1.PodTemplateSpecArgs{
+		Metadata: k8s.TemplateMetadata(args.app.jobName),
+		Spec: &corev1.PodSpecArgs{
+			Containers: postgresBackupContainerSpec(args),
+			Volumes: k8s.CreateVolumeSpec(
+				args.secretName,
+				args.app.volumeName,
+				[]*k8s.VolumeItemsProperties{
+					{
+						Key:   "gcsbucket.credentials",
+						Value: "credentials.json",
+					},
+				},
+			),
+		},
+	}
+}
+
 func createJobSpec(args *specProperties) *batchv1.JobArgs {
 	return &batchv1.JobArgs{
 		Metadata: k8s.Metadata(args.namespace, args.app.jobName),
 		Spec: batchv1.JobSpecArgs{
 			Template: jobPodTemplate(args),
 			Selector: k8s.SpecLabelSelector(args.app.jobName),
+		},
+	}
+}
+
+func createPostgresCronJobSpec(args *specProperties) *batchv1.CronJobArgs {
+	return &batchv1.CronJobArgs{
+		Metadata: k8s.Metadata(args.namespace, args.app.jobName),
+		Spec: batchv1.CronJobSpecArgs{
+			Schedule: pulumi.String(args.app.schedule),
+			JobTemplate: &batchv1.JobTemplateSpecArgs{
+				Spec: &batchv1.JobSpecArgs{
+					Template: createPostgresPodTemplateSpecArgs(args),
+				},
+			},
 		},
 	}
 }
