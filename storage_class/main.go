f8b718a8d221807766c359dcbea1e4077c19288d
Siddhartha Basu <biosidd@gmail.com>
2024-09-09 16:36:22 -0500

    feat(storage_class): add Pulumi configuration for Kubernetes storage class
    This commit introduces a new Pulumi project for managing Kubernetes
    storage classes. It includes the necessary Pulumi configuration files
    and a Go program that defines and creates a storage class based on
    configuration parameters. This setup allows for dynamic provisioning of
    storage resources in a Kubernetes cluster, enhancing the deployment and
    management of persistent storage.

---
+++ storage_class/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	storagev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/storage/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+type StorageClassConfig struct {
+	DiskType    string
+	Name        string
+	Provisioner string
+}
+
+func main() {
+	pulumi.Run(run)
+}
+
+func run(ctx *pulumi.Context) error {
+	storageConfig, err := NewStorageClassConfig(ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := storageConfig.CreateStorageClass(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewStorageClassConfig(ctx *pulumi.Context) (*StorageClassConfig, error) {
+	conf := config.New(ctx, "storage-class")
+	storageConfig := &StorageClassConfig{}
+	if err := conf.TryObject("", storageConfig); err != nil {
+		return nil, fmt.Errorf("failed to read storage class config: %w", err)
+	}
+	return storageConfig, nil
+}
+
+func (sconf *StorageClassConfig) CreateStorageClass(ctx *pulumi.Context) error {
+	_, err := storagev1.NewStorageClass(
+		ctx,
+		sconf.Name,
+		&storagev1.StorageClassArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String(sconf.Name),
+			},
+			Provisioner: pulumi.String(sconf.Provisioner),
+			Parameters: pulumi.StringMap{
+				"type": pulumi.String(sconf.DiskType),
+			},
+			AllowVolumeExpansion: pulumi.Bool(true),
+			VolumeBindingMode:    pulumi.String("WaitForFirstConsumer"),
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create storage class: %w", err)
+	}
+	return nil
+}
