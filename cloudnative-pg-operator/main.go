baa4fb56c8a727bb98c61071a7ed7bd1d22d0256
Siddhartha Basu <biosidd@gmail.com>
2024-09-11 12:36:29 -0500

    feat(main.go): integrate Helm for Kubernetes resource management in cloudnative-pg-operator
    This change introduces the use of the Helm package from the Pulumi
    Kubernetes SDK to manage Kubernetes resources for the PostgreSQL
    operator. By integrating Helm, the deployment process is streamlined,
    allowing for easier updates and management of the PostgreSQL operator
    through Helm charts. Additionally, a new `PgOperator` struct
    is introduced to encapsulate the operator's configuration and
    installation logic, further organizing the codebase and improving the
    separation of concerns.

--- cloudnative-pg-operator/main.go
+++ cloudnative-pg-operator/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
@@ -19,6 +20,10 @@ type PgOperatorConfig struct {
 	Namespace string
 }
 
+type PgOperator struct {
+	config PgOperatorConfig
+}
+
 func main() {
 	pulumi.Run(run)
 }
@@ -29,24 +34,43 @@ func run(ctx *pulumi.Context) error {
 		return err
 	}
 
-	if err := logNamespace(ctx, pgConfig.Namespace); err != nil {
-		return err
-	}
-
-	// Add more operations using pgConfig here
-
-	return nil
+	pgOperator := NewPgOperator(pgConfig)
+	return pgOperator.Install(ctx)
 }
 
 func loadConfig(ctx *pulumi.Context) (PgOperatorConfig, error) {
 	conf := config.New(ctx, "pg-operator")
 	var pgConfig PgOperatorConfig
 	if err := conf.TryObject("properties", &pgConfig); err != nil {
-		return PgOperatorConfig{}, fmt.Errorf("failed to read pg-operator properties: %w", err)
+		return PgOperatorConfig{}, fmt.Errorf(
+			"failed to read pg-operator properties: %w",
+			err,
+		)
 	}
 	return pgConfig, nil
 }
 
-func logNamespace(ctx *pulumi.Context, namespace string) error {
-	return ctx.Log.Info(fmt.Sprintf("Namespace: %s", namespace), nil)
+func NewPgOperator(config PgOperatorConfig) *PgOperator {
+	return &PgOperator{config: config}
+}
+
+func (pg *PgOperator) Install(ctx *pulumi.Context) error {
+	// Install the Helm chart
+	_, err := helm.NewRelease(ctx, "cloudnative-pg-operator", &helm.ReleaseArgs{
+		Chart:     pulumi.String(pg.config.Chart.Name),
+		Version:   pulumi.String(pg.config.Chart.Version),
+		Namespace: pulumi.String(pg.config.Namespace),
+		RepositoryOpts: helm.RepositoryOptsArgs{
+			Repo: pulumi.String(pg.config.Chart.Repository),
+		},
+		Values: pulumi.Map{
+			"image": pulumi.Map{
+				"tag": pulumi.String(pg.config.Image.Tag),
+			},
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to install Helm chart: %w", err)
+	}
+	return nil
 }
