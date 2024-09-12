3eb1879d7487e8c81ce3eaf440c68d7289d82a4a
Siddhartha Basu <biosidd@gmail.com>
2024-09-12 14:48:06 -0500

    feat(main.go): introduce structured types for cluster configuration and add PostgreSQL cluster creation
    This change introduces new structured types (`Image`, `Storage`,
    `Cluster`, `Secret`) to better organize the configuration data for a
    PostgreSQL cluster in the Pulumi project. This makes the configuration
    more readable and maintainable. Additionally, the
    `CreatePostgresCluster` function is added to facilitate the creation of
    a PostgreSQL cluster using the defined properties, improving the
    automation capabilities of the infrastructure setup.

--- cloudnative-pg-cluster/main.go
+++ cloudnative-pg-cluster/main.go
@@ -8,24 +8,46 @@ import (
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+
+	v1 "github.com/dictybase-docker/cluster-ops/crds/kubernetes/postgresql/v1"
 )
 
+type Image struct {
+	Name string `pulumi:"name"`
+	Tag  string `pulumi:"tag"`
+}
+
+type Storage struct {
+	Class string `pulumi:"class"`
+	Size  string `pulumi:"size"`
+}
+
+type Cluster struct {
+	Image     Image   `pulumi:"image"`
+	Instances int     `pulumi:"instances"`
+	Name      string  `pulumi:"name"`
+	Storage   Storage `pulumi:"storage"`
+}
+
+type Secret struct {
+	Filepath string `pulumi:"filepath"`
+	Key      string `pulumi:"key"`
+	Name     string `pulumi:"name"`
+}
+
 type Properties struct {
-	Namespace string `pulumi:"namespace"`
-	Secret    struct {
-		Filepath string `pulumi:"filepath"`
-		Key      string `pulumi:"key"`
-		Name     string `pulumi:"name"`
-	} `pulumi:"secret"`
+	Namespace string  `pulumi:"namespace"`
+	Cluster   Cluster `pulumi:"cluster"`
+	Secret    Secret  `pulumi:"secret"`
 }
 
 func NewProperties(ctx *pulumi.Context) (*Properties, error) {
-	var props Properties
+	props := &Properties{}
 	cfg := config.New(ctx, "")
-	if err := cfg.TryObject("properties", &props); err != nil {
+	if err := cfg.TryObject("properties", props); err != nil {
 		return nil, fmt.Errorf("failed to read config: %w", err)
 	}
-	return &props, nil
+	return props, nil
 }
 
 func (prop *Properties) CreateSecret(
@@ -67,10 +89,70 @@ func createResources(ctx *pulumi.Context) error {
 		return err
 	}
 
-	// Export the secret name
+	// Create the PostgreSQL Cluster, passing the secret as a dependency
+	cluster, err := props.CreatePostgresCluster(ctx, secret)
+	if err != nil {
+		return err
+	}
+
+	// Export the secret name and cluster name
 	ctx.Export("secretName", secret.Metadata.Name())
+	ctx.Export("clusterName", cluster.Metadata.Name())
 
 	return nil
+}
+
+func (prop *Properties) CreatePostgresCluster(
+	ctx *pulumi.Context,
+	secret *corev1.Secret,
+) (*v1.Cluster, error) {
+	clusterArgs := prop.buildClusterArgs()
+	cluster, err := v1.NewCluster(
+		ctx, prop.Cluster.Name,
+		clusterArgs,
+		pulumi.DependsOn([]pulumi.Resource{secret}),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create PostgreSQL cluster: %w", err)
+	}
+	return cluster, nil
+}
+
+func (prop *Properties) buildClusterArgs() *v1.ClusterArgs {
+	return &v1.ClusterArgs{
+		ApiVersion: pulumi.String("postgresql.cnpg.io/v1"),
+		Kind:       pulumi.String("Cluster"),
+		Metadata:   prop.buildMetadata(),
+		Spec:       prop.buildClusterSpec(),
+	}
+}
+
+func (prop *Properties) buildMetadata() *metav1.ObjectMetaArgs {
+	return &metav1.ObjectMetaArgs{
+		Name:      pulumi.String(prop.Cluster.Name),
+		Namespace: pulumi.String(prop.Namespace),
+	}
+}
+
+func (prop *Properties) buildClusterSpec() *v1.ClusterSpecArgs {
+	return &v1.ClusterSpecArgs{
+		Instances: pulumi.Int(prop.Cluster.Instances),
+		ImageName: pulumi.String(
+			fmt.Sprintf(
+				"%s:%s",
+				prop.Cluster.Image.Name,
+				prop.Cluster.Image.Tag,
+			),
+		),
+		Storage: prop.buildStorageArgs(),
+	}
+}
+
+func (prop *Properties) buildStorageArgs() *v1.ClusterSpecStorageArgs {
+	return &v1.ClusterSpecStorageArgs{
+		StorageClass: pulumi.String(prop.Cluster.Storage.Class),
+		Size:         pulumi.String(prop.Cluster.Storage.Size),
+	}
 }
 
 func main() {
