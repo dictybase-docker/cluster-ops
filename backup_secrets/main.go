c0c16672a44de0cca2cfd6e91e5369201455ce2a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-09-22 12:38:32 -0500

    feat(backup_secrets): add initial implementation for backup secrets management
    Introduce a new Go module for managing backup secrets using Pulumi. This
    module allows users to define and create Kubernetes secrets for backup
    purposes, including the ability to read configuration from a Pulumi
    stack.

---
+++ backup_secrets/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+type BackupSecretsConfig struct {
+	Secret struct {
+		Name       string
+		ResticPass string
+	}
+	Namespace string
+}
+
+type BackupSecrets struct {
+	Config *BackupSecretsConfig
+}
+
+func ReadConfig(ctx *pulumi.Context) (*BackupSecretsConfig, error) {
+	conf := config.New(ctx, "backup_secrets")
+	backupConfig := &BackupSecretsConfig{}
+	if err := conf.TryObject("properties", backupConfig); err != nil {
+		return nil, fmt.Errorf(
+			"failed to read backup_secrets config: %w",
+			err,
+		)
+	}
+	return backupConfig, nil
+}
+
+func NewBackupSecrets(config *BackupSecretsConfig) *BackupSecrets {
+	return &BackupSecrets{
+		Config: config,
+	}
+}
+
+func (bsr *BackupSecrets) Install(ctx *pulumi.Context) error {
+	secret, err := corev1.NewSecret(
+		ctx,
+		bsr.Config.Secret.Name,
+		&corev1.SecretArgs{
+			Metadata: bsr.createMetadata(),
+			StringData: pulumi.StringMap{
+				"resticPass": pulumi.String(bsr.Config.Secret.ResticPass),
+			},
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("error creating backup secret: %w", err)
+	}
+
+	ctx.Export("secretName", secret.Metadata.Name())
+	return nil
+}
+
+func (bsr *BackupSecrets) createMetadata() *metav1.ObjectMetaArgs {
+	return &metav1.ObjectMetaArgs{
+		Name:      pulumi.String(bsr.Config.Secret.Name),
+		Namespace: pulumi.String(bsr.Config.Namespace),
+	}
+}
+
+func Run(ctx *pulumi.Context) error {
+	backupConfig, err := ReadConfig(ctx)
+	if err != nil {
+		return err
+	}
+
+	backupSecrets := NewBackupSecrets(backupConfig)
+
+	if err := backupSecrets.Install(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	pulumi.Run(Run)
+}
