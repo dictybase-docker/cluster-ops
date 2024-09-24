99c3c7ff63da4d7dcfc018617fe31e82542ac90b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-09-24 18:35:58 -0500

    feat(flags.go): add environment variable support for various flags to enhance configuration flexibility
    The changes introduce environment variable mappings for several flags,
    allowing users to configure the application using environment variables.

--- internal/kops/flags.go
+++ internal/kops/flags.go
@@ -20,6 +20,7 @@ func DefineOtherFlags() []cli.Flag {
 			Name:    "image",
 			Aliases: []string{"im"},
 			Usage:   "compute image to be used for the vm",
+			EnvVars: []string{"COMPUTE_IMAGE"},
 			Value:   "ubuntu-os-cloud/ubuntu-2204-jammy-v20240829",
 		},
 	}
@@ -32,18 +33,21 @@ func DefineNodeFlags() []cli.Flag {
 			Aliases: []string{"ns"},
 			Usage:   "The machine type for kubernetes node",
 			Value:   "n1-custom-2-4096",
+			EnvVars: []string{"NODE_MACHINE"},
 		},
 		&cli.IntFlag{
 			Name:    "node-volume-size",
 			Aliases: []string{"nvs"},
 			Usage:   "size of the boot disk of kubernetes nodes in GB",
 			Value:   100,
+			EnvVars: []string{"NODE_DISK_SIZE"},
 		},
 		&cli.IntFlag{
 			Name:    "node-count",
 			Aliases: []string{"nc"},
 			Usage:   "no of kubernetes worker node to create",
 			Value:   4,
+			EnvVars: []string{"TOTAL_NODES"},
 		},
 	}
 }
@@ -55,18 +59,21 @@ func DefineMasterFlags() []cli.Flag {
 			Aliases: []string{"ms"},
 			Usage:   "The machine type for kubernetes master",
 			Value:   "n1-custom-4-8192",
+			EnvVars: []string{"MASTER_MACHINE"},
 		},
 		&cli.StringFlag{
 			Name:    "master-count",
 			Aliases: []string{"mc"},
 			Usage:   "no of kubernetes master to create",
 			Value:   "1",
+			EnvVars: []string{"TOTAL_MASTER"},
 		},
 		&cli.Int64Flag{
 			Name:    "master-volume-size",
 			Aliases: []string{"mvs"},
 			Usage:   "size of the boot disk of kubernetes master in GB",
 			Value:   75,
+			EnvVars: []string{"MASTER_DISK_SIZE"},
 		},
 	}
 }
@@ -100,6 +107,7 @@ func DefineCredentialsFlags() []cli.Flag {
 			Name:    "ssh-key",
 			Aliases: []string{"sk"},
 			Usage:   "public ssh key file that will be injected in the vm for login",
+			EnvVars: []string{"SSH_KEY"},
 			Value:   "credentials/k8sVM.pub",
 		},
 	}
