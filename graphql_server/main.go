c5e20a9689e81b1b4813610551519b6a6d314c6f
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-25 11:22:04 -0500

    feat(main.go): refactor execute function to use updated field names in Config struct and create deployment and service with correct field values
    The changes were made to improve the consistency and readability of the code by using lowercase field names for the Config struct. Additionally, the loadConfig function was updated to directly return a Config struct instead of a pointer. Support was added to load additional configuration values like tag, logLevel, port, secretName, configMapName, s3Bucket, and s3BucketPath. The execute function was refactored to use the updated field names in the Config struct and create deployment and service with the correct field values.

--- graphql_server/main.go
+++ graphql_server/main.go
@@ -21,139 +21,113 @@ var allowedOrigins = []string{
 }
 
 type Config struct {
-	Namespace     string
-	Name          string
-	Image         string
-	Tag           string
-	LogLevel      string
-	Port          int
-	SecretName    string
-	ConfigMapName string
-	S3Bucket      string
-	S3BucketPath  string
-  AllowedOrigins []string
+	namespace     string
+	name          string
+	image         string
+	tag           string
+	logLevel      string
+	port          int
+	secretName    string
+	configMapName string
+	s3Bucket      string
+	s3BucketPath  string
+  allowedOrigins []string
 }
 
 func main() {
 	pulumi.Run(execute)
 }
 
-func loadConfig(ctx *pulumi.Context) (*Config, error) {
+func loadConfig(ctx *pulumi.Context) Config {
 	conf := config.New(ctx, "")
 
 	namespace := conf.Require("namespace")
 	name := conf.Require("name")
+	tag := conf.Require("tag")
+
 	image := conf.Get("image")
 	if image == "" {
 		image = "dictybase/graphql-server"
 	}
-	tag := conf.Require("tag")
+
 	logLevel := conf.Get("logLevel")
 	if logLevel == "" {
 		logLevel = "error"
 	}
+  
 	port := conf.GetInt("port")
 	if port == 0 {
 		port = 8080
 	}
+  
 	secretName := conf.Get("secret")
 	if secretName == "" {
 		secretName = "dictycr-secret"
 	}
+
 	configMapName := conf.Get("configMap")
 	if configMapName == "" {
 		configMapName = "dictycr-configuration"
 	}
+
 	s3Bucket := conf.Get("s3Bucket")
 	if s3Bucket == "" {
 		s3Bucket = "editor"
 	}
+
 	s3BucketPath := conf.Get("s3BucketPath")
 	if s3BucketPath == "" {
 		s3BucketPath = "assets"
 	}
 
-	return &Config{
-		Namespace:     namespace,
-		Name:          name,
-		Image:         image,
-		Tag:           tag,
-		LogLevel:      logLevel,
-		Port:          port,
-		SecretName:    secretName,
-		ConfigMapName: configMapName,
-		S3Bucket:      s3Bucket,
-		S3BucketPath:  s3BucketPath,
-		AllowedOrigins: allowedOrigins,
-	}, nil
+	return Config{
+		namespace:     namespace,
+		name:          name,
+		image:         image,
+		tag:           tag,
+		logLevel:      logLevel,
+		port:          port,
+		secretName:    secretName,
+		configMapName: configMapName,
+		s3Bucket:      s3Bucket,
+		s3BucketPath:  s3BucketPath,
+		allowedOrigins: allowedOrigins,
+	}
 }
 
 func execute(ctx *pulumi.Context) error {
 	// Load configuration
-	config, err := loadConfig(ctx)
-	if err != nil {
-		return err
-	}
-
+	config := loadConfig(ctx)
 
 	// Create deployment
-	_, err = appsv1.NewDeployment(ctx, fmt.Sprintf("%s-api-server", config.Name), &appsv1.DeploymentArgs{
+  _, err := appsv1.NewDeployment(ctx, fmt.Sprintf("%s-api-server", config.name), &appsv1.DeploymentArgs{
 		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(fmt.Sprintf("%s-api-server", config.Name)),
-			Namespace: pulumi.String(config.Namespace),
+			Name:      pulumi.String(fmt.Sprintf("%s-api-server", config.name)),
+			Namespace: pulumi.String(config.namespace),
 		},
 		Spec: &appsv1.DeploymentSpecArgs{
 			Selector: &metav1.LabelSelectorArgs{
 				MatchLabels: pulumi.StringMap{
-					"app": pulumi.String(fmt.Sprintf("%s-api-server", config.Name)),
+					"app": pulumi.String(fmt.Sprintf("%s-api-server", config.name)),
 				},
 			},
 			Template: &corev1.PodTemplateSpecArgs{
 				Metadata: &metav1.ObjectMetaArgs{
 					Labels: pulumi.StringMap{
-						"app": pulumi.String(fmt.Sprintf("%s-api-server", config.Name)),
+						"app": pulumi.String(fmt.Sprintf("%s-api-server", config.name)),
 					},
 				},
 				Spec: &corev1.PodSpecArgs{
-					Containers: corev1.ContainerArray{
-						&corev1.ContainerArgs{
-							Name:  pulumi.String(fmt.Sprintf("%s-container", config.Name)),
-							Image: pulumi.String(fmt.Sprintf("%s:%s", config.Image, config.Tag)),
-							Args: pulumi.StringArray{
-								pulumi.String("--log-level"),
-								pulumi.String(config.LogLevel),
-								pulumi.String("start-server"),
-							},
-							Env: corev1.EnvVarArray{
-								&corev1.EnvVarArgs{
-									Name: pulumi.String("SECRET_KEY"),
-									ValueFrom: &corev1.EnvVarSourceArgs{
-										SecretKeyRef: &corev1.SecretKeySelectorArgs{
-											Name: pulumi.String(config.SecretName),
-											Key:  pulumi.String("minio.secretkey"),
-										},
-									},
-								},
-								&corev1.EnvVarArgs{
-									Name: pulumi.String("ACCESS_KEY"),
-									ValueFrom: &corev1.EnvVarSourceArgs{
-										SecretKeyRef: &corev1.SecretKeySelectorArgs{
-											Name: pulumi.String(config.SecretName),
-											Key:  pulumi.String("minio.accesskey"),
-										},
-									},
-								},
-								// Add other environment variables here
-							},
-							Ports: corev1.ContainerPortArray{
-								&corev1.ContainerPortArgs{
-									Name:          pulumi.String(fmt.Sprintf("%s-api", config.Name)),
-									ContainerPort: pulumi.Int(config.Port),
-									Protocol:      pulumi.String("TCP"),
-								},
-							},
-						},
-					},
+					Containers: containerArray(&ContainerConfig{
+            name: config.name,
+            image: config.image,
+            tag: config.tag,
+            logLevel: config.logLevel,
+            configMapName: config.logLevel,
+            secretName: config.secretName,
+            port: config.port,
+            allowedOrigins: config.allowedOrigins,
+          }),
 				},
 			},
 		},
@@ -163,27 +137,28 @@ func execute(ctx *pulumi.Context) error {
 	}
 
 	// Create service
-	//_, err = corev1.NewService(ctx, fmt.Sprintf("%s-api", config.Name), &corev1.ServiceArgs{
-	//	Metadata: &metav1.ObjectMetaArgs{
-	//		Name:      pulumi.String(fmt.Sprintf("%s-api", config.Name)),
-	//		Namespace: pulumi.String(config.Namespace),
-	//	},
-	//	Spec: &corev1.ServiceSpecArgs{
-	//		Selector: pulumi.StringMap{
-	//			"app": pulumi.String(fmt.Sprintf("%s-api-server", config.Name)),
-	//		},
-	//		Ports: corev1.ServicePortArray{
-	//			&corev1.ServicePortArgs{
-	//				Port:       pulumi.Int(config.Port),
-	//				TargetPort: pulumi.Int(config.Port),
-	//			},
-	//		},
-	//		Type: pulumi.String("NodePort"),
-	//	},
-	//})
-	//if err != nil {
-	//	return err
-	//}
+	_, err = corev1.NewService(ctx, fmt.Sprintf("%s-api", config.name), &corev1.ServiceArgs{
+		Metadata: &metav1.ObjectMetaArgs{
+			Name:      pulumi.String(fmt.Sprintf("%s-api", config.name)),
+			Namespace: pulumi.String(config.namespace),
+		},
+		Spec: &corev1.ServiceSpecArgs{
+			Selector: pulumi.StringMap{
+				"app": pulumi.String(fmt.Sprintf("%s-api-server", config.name)),
+			},
+			Ports: corev1.ServicePortArray{
+				&corev1.ServicePortArgs{
+					Port:       pulumi.Int(config.port),
+					TargetPort: pulumi.Int(config.port),
+				},
+			},
+			Type: pulumi.String("NodePort"),
+		},
+	})
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
+
