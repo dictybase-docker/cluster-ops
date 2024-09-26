4b98fafe0f00e545115379a40d839188b4f83f46
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-26 09:28:23 -0500

    feat(deployment.go): add functions to create deployment metadata and spec for the GraphQL server
    The functions CreateDeploymentMetaData and CreateDeploymentSpec have been added to the GraphqlServer struct in deployment.go. These functions are responsible for generating the metadata and spec required for creating a deployment for the GraphQL server. By modularizing this logic into separate functions, the codebase becomes more organized and easier to maintain.

---
+++ graphql_server/deployment.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func (gs *GraphqlServer) CreateDeploymentMetaData() (*metav1.ObjectMetaArgs) {
+	deploymentName := fmt.Sprintf("%s-api-server", gs.Config.Name)
+  return &metav1.ObjectMetaArgs{
+			Name:      pulumi.String(deploymentName),
+			Namespace: pulumi.String(gs.Config.Namespace),
+  }
+}
+
+func (gs *GraphqlServer) CreateDeploymentSpec() (*appsv1.DeploymentSpecArgs) {
+	deploymentName := fmt.Sprintf("%s-api-server", gs.Config.Name)
+  return &appsv1.DeploymentSpecArgs{
+			Selector: &metav1.LabelSelectorArgs{
+				MatchLabels: pulumi.StringMap{
+					"app": pulumi.String(deploymentName),
+				},
+			},
+			Template: &corev1.PodTemplateSpecArgs{
+				Metadata: &metav1.ObjectMetaArgs{
+					Labels: pulumi.StringMap{
+						"app": pulumi.String(deploymentName),
+					},
+				},
+				Spec: &corev1.PodSpecArgs{
+					Containers: gs.ContainerArray(),
+				},
+			},
+		}
+}
+
+func (gs *GraphqlServer) CreateDeployment(ctx *pulumi.Context) (*appsv1.Deployment, error) {
+	deployment, err := appsv1.NewDeployment(ctx, fmt.Sprintf("%s-api-server", gs.Config.Name), &appsv1.DeploymentArgs{
+		Metadata: gs.CreateDeploymentMetaData(),
+    Spec: gs.CreateDeploymentSpec(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return deployment, nil
+}
+
