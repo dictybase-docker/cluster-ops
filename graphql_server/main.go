df5e3fad743e81b53f39f78f1bf236ee275f31e9
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-26 14:06:05 -0500

    refactor(main.go): restructure GraphqlServerConfig to include additional authentication and storage configurations
    The GraphqlServerConfig struct has been expanded to include new fields for authentication and storage configurations such as AuthAppId, AuthAppSecret, AuthEndpoint, JwksURI, JwtAudience, JwtIssuer, MinioAccess, MinioSecret, OrganismEndpoint, PublicationApiEndpoint, and S3StorageEndpoint. This restructuring enhances the flexibility and extensibility of the configuration options for the GraphQL server.

--- graphql_server/main.go
+++ graphql_server/main.go
@@ -6,18 +6,39 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
+type SecretConfig struct {
+  name string
+  key string
+}
+
+type ConfigMapEntry struct {
+  name string
+  key string
+}
+
 type GraphqlServerConfig struct {
-	Namespace     string
-	Name          string
-	Image         string
-	Tag           string
-	LogLevel      string
-	Port          int
-	SecretName    string
-	ConfigMapName string
-	S3Bucket      string
-	S3BucketPath  string
-  AllowedOrigins []string
+	Namespace              string
+	Name                   string
+	Image                  string
+	Tag                    string
+	LogLevel               string
+	Port                   int
+	SecretName             string
+	ConfigMapName          string
+	S3Bucket               string
+	S3BucketPath           string
+	AllowedOrigins         []string
+	AuthAppId              SecretConfig
+	AuthAppSecret          SecretConfig
+	AuthEndpoint           ConfigMapEntry
+	JwksURI                SecretConfig
+	JwtAudience            SecretConfig
+	JwtIssuer              SecretConfig
+	MinioAccess            SecretConfig
+	MinioSecret            SecretConfig
+	OrganismEndpoint       ConfigMapEntry
+	PublicationApiEndpoint ConfigMapEntry
+	S3StorageEndpoint      ConfigMapEntry
 }
 
 type GraphqlServer struct {
