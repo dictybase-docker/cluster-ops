43b1785bda3e56270e9718e6388b62d59aa8c6a3
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-26 18:56:19 -0500

    feat(main.go): add EventMessengerEmail struct to represent email configuration for event messenger
    The EventMessengerEmail struct is added to the main.go file to represent the email configuration for the event messenger. This struct will hold the configuration settings related to sending emails within the event messenger application.

--- event-messenger/main.go
+++ event-messenger/main.go
@@ -4,6 +4,10 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+type EventMessengerEmail struct {
+	Config *EventMessengerEmailConfig
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
