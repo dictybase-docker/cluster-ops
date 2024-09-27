36e7c8b7906f99f463deb9d2e892c6ecb6dc78b6
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-27 15:08:45 -0500

    feat(container.go): add new functions ContainerArgs and ContainerArray to generate container arguments and array
    The changes made in this commit involve fixing the capitalization of struct fields in the ConfigMapKeySelectorArgs struct to match the struct definition. Additionally, two new functions, ContainerArgs and ContainerArray, have been added to generate container arguments and an array of containers, respectively. These changes improve code readability and maintainability by following consistent naming conventions and separating concerns into smaller, more focused functions.

--- event-messenger/container.go
+++ event-messenger/container.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -23,8 +24,8 @@ func (eme *EventMessengerEmail) ConfigMapEnvArgsArray() corev1.EnvVarArray {
 			Name: pulumi.String(envVar.name),
 			ValueFrom: &corev1.EnvVarSourceArgs{
 				ConfigMapKeyRef: &corev1.ConfigMapKeySelectorArgs{
-					Name: pulumi.String(envVar.configMap.name),
-					Key:  pulumi.String(envVar.configMap.key),
+					Name: pulumi.String(envVar.configMap.Name),
+					Key:  pulumi.String(envVar.configMap.Key),
 				},
 			},
 		})
@@ -38,8 +39,8 @@ func (eme *EventMessengerEmail) SecretEnvArgsArray() corev1.EnvVarArray {
 			Name: pulumi.String("MAILGUN_API_KEY"),
 			ValueFrom: &corev1.EnvVarSourceArgs{
 				ConfigMapKeyRef: &corev1.ConfigMapKeySelectorArgs{
-					Name: pulumi.String(eme.Config.MailgunApiKey.name),
-					Key:  pulumi.String(eme.Config.MailgunApiKey.key),
+					Name: pulumi.String(eme.Config.MailgunApiKey.Name),
+					Key:  pulumi.String(eme.Config.MailgunApiKey.Key),
 				},
 			},
     },
@@ -51,5 +52,40 @@ func (eme *EventMessengerEmail) ContainerEnvArgsArray() corev1.EnvVarArray {
 	envVarArray = append(envVarArray, eme.ConfigMapEnvArgsArray()...)
 	envVarArray = append(envVarArray, eme.SecretEnvArgsArray()...)
 	return envVarArray
+}
+
+func (eme *EventMessengerEmail) ContainerArgs() pulumi.StringArray {
+  args := []string{
+    "--log-level",
+    eme.Config.LogLevel,
+    "send-email",
+    "--subject",
+    eme.Config.Nats.Subject,
+    "--domain",
+    "$(EMAIL_DOMAIN)",
+    "--apiKey",
+    "$(MAILGUN_API_KEY)",
+    "--name",
+    "$(EMAIL_SENDER_NAME)",
+    "--sender",
+    "$(EMAIL_SENDER)",
+    "--cc",
+    "$(EMAIL_CC)",
+    "--pub",
+    "$(PUBLICATION_API_ENDPOINT)",
+  }
+  return pulumi.ToStringArray(args)
+}
+
+func (eme *EventMessengerEmail) ContainerArray() corev1.ContainerArray {
+	config := eme.Config
+	return corev1.ContainerArray{
+		&corev1.ContainerArgs{
+			Name:  pulumi.String(fmt.Sprintf("%s-container", config.Namespace)),
+			Image: pulumi.String(fmt.Sprintf("%s:%s", config.Image, config.Image.Tag)),
+      Args: eme.ContainerArgs(),
+			Env:   eme.ContainerEnvArgsArray(),
+		},
+	}
 }
 
