fdc17b5179880115a1f49cb79ff6875004b93c02
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-09-27 06:04:24 -0500

    feat(redis): refactor Redis standalone deployment to use Pulumi Kubernetes SDK v4 for better maintainability and functionality
    The code now utilizes the Pulumi Kubernetes SDK v4, which provides a
    more modern and efficient way to manage Kubernetes resources. This
    refactor includes creating persistent volume claims, deployments, and
    services using the new SDK, enhancing the overall structure and
    readability of the code.

--- redis-standalone/main.go
+++ redis-standalone/main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 
-	redisv1beta2 "github.com/dictybase-docker/cluster-ops/crds/kubernetes/redis/v1beta2"
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
-
-type StorageResourcesArgs = redisv1beta2.RedisSpecStorageVolumeClaimTemplateSpecResourcesArgs
 
 type RedisStandaloneConfig struct {
 	Image struct {
 		Name string
 		Tag  string
 	}
+	Name      string
 	Namespace string
 	Storage   struct {
 		Class string
@@ -28,7 +28,7 @@ type RedisStandalone struct {
 }
 
 func ReadConfig(ctx *pulumi.Context) (*RedisStandaloneConfig, error) {
-	conf := config.New(ctx, "redis-standalone")
+	conf := config.New(ctx, "")
 	redisConfig := &RedisStandaloneConfig{}
 	if err := conf.TryObject("properties", redisConfig); err != nil {
 		return nil, fmt.Errorf(
@@ -46,93 +46,183 @@ func NewRedisStandalone(config *RedisStandaloneConfig) *RedisStandalone {
 }
 
 func (rds *RedisStandalone) Install(ctx *pulumi.Context) error {
-	redis, err := redisv1beta2.NewRedis(
-		ctx,
-		"redis-standalone",
-		&redisv1beta2.RedisArgs{
-			Metadata: rds.createMetadata(),
-			Spec:     rds.createRedisSpec(),
-		},
-	)
+	pvc, err := rds.createPersistentVolumeClaim(ctx)
 	if err != nil {
-		return fmt.Errorf("error creating Redis standalone: %w", err)
+		return err
 	}
 
-	ctx.Export("redisName", redis.Metadata.Name())
+	err = rds.createDeployment(ctx, pvc)
+	if err != nil {
+		return err
+	}
+
+	err = rds.createService(ctx)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (rds *RedisStandalone) createMetadata() *metav1.ObjectMetaArgs {
-	return &metav1.ObjectMetaArgs{
-		Name:      pulumi.String("redis-standalone"),
-		Namespace: pulumi.String(rds.Config.Namespace),
+func (rds *RedisStandalone) createService(ctx *pulumi.Context) error {
+	_, err := corev1.NewService(ctx, rds.Config.Name, &corev1.ServiceArgs{
+		Metadata: rds.createMetadata(),
+		Spec: &corev1.ServiceSpecArgs{
+			Selector: rds.createLabels(),
+			Ports: corev1.ServicePortArray{
+				&corev1.ServicePortArgs{
+					Port:       pulumi.Int(6379),
+					TargetPort: pulumi.Int(6379),
+				},
+			},
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("error creating Service: %w", err)
 	}
+	return nil
 }
 
-func (rds *RedisStandalone) createRedisSpec() *redisv1beta2.RedisSpecArgs {
-	return &redisv1beta2.RedisSpecArgs{
-		Storage:          rds.createStorageSpec(),
-		KubernetesConfig: rds.createKubernetesConfig(),
-		LivenessProbe:    rds.createLivenessProbe(),
-		ReadinessProbe:   rds.createReadinessProbe(),
-		SecurityContext:  rds.createSecurityContext(),
-	}
-}
-
-func (rds *RedisStandalone) createSecurityContext() *redisv1beta2.RedisSpecSecurityContextArgs {
-	return &redisv1beta2.RedisSpecSecurityContextArgs{
-		RunAsUser: pulumi.Int(1000),
-	}
-}
-
-func (rds *RedisStandalone) createStorageSpec() *redisv1beta2.RedisSpecStorageArgs {
-	return &redisv1beta2.RedisSpecStorageArgs{
-		VolumeClaimTemplate: &redisv1beta2.RedisSpecStorageVolumeClaimTemplateArgs{
-			Spec: &redisv1beta2.RedisSpecStorageVolumeClaimTemplateSpecArgs{
+func (rds *RedisStandalone) createPersistentVolumeClaim(
+	ctx *pulumi.Context,
+) (*corev1.PersistentVolumeClaim, error) {
+	pvc, err := corev1.NewPersistentVolumeClaim(
+		ctx,
+		fmt.Sprintf("%s-data", rds.Config.Name),
+		&corev1.PersistentVolumeClaimArgs{
+			Metadata: rds.createMetadata(),
+			Spec: &corev1.PersistentVolumeClaimSpecArgs{
 				AccessModes: pulumi.StringArray{
 					pulumi.String("ReadWriteOnce"),
 				},
 				StorageClassName: pulumi.String(rds.Config.Storage.Class),
-				Resources:        rds.createStorageResources(),
+				Resources: &corev1.VolumeResourceRequirementsArgs{
+					Requests: pulumi.StringMap{
+						"storage": pulumi.String(rds.Config.Storage.Size),
+					},
+				},
+			},
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error creating PersistentVolumeClaim: %w", err)
+	}
+	return pvc, nil
+}
+
+func (rds *RedisStandalone) createDeployment(
+	ctx *pulumi.Context,
+	pvc *corev1.PersistentVolumeClaim,
+) error {
+	_, err := appsv1.NewDeployment(ctx, rds.Config.Name, &appsv1.DeploymentArgs{
+		Metadata: &metav1.ObjectMetaArgs{
+			Name:      pulumi.String(rds.Config.Name),
+			Namespace: pulumi.String(rds.Config.Namespace),
+			Labels:    rds.createLabels(),
+		},
+		Spec: rds.createDeploymentSpec(pvc),
+	})
+	if err != nil {
+		return fmt.Errorf("error creating Deployment: %w", err)
+	}
+	return nil
+}
+
+func (rds *RedisStandalone) createDeploymentSpec(
+	pvc *corev1.PersistentVolumeClaim,
+) *appsv1.DeploymentSpecArgs {
+	return &appsv1.DeploymentSpecArgs{
+		Replicas: pulumi.Int(1),
+		Selector: &metav1.LabelSelectorArgs{
+			MatchLabels: rds.createLabels(),
+		},
+		Template: &corev1.PodTemplateSpecArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Labels: rds.createLabels(),
+			},
+			Spec: rds.createPodSpec(pvc),
+		},
+	}
+}
+
+func (rds *RedisStandalone) createPodSpec(
+	pvc *corev1.PersistentVolumeClaim,
+) *corev1.PodSpecArgs {
+	return &corev1.PodSpecArgs{
+		InitContainers: corev1.ContainerArray{
+			rds.createInitContainer(),
+		},
+		Containers: corev1.ContainerArray{
+			rds.createRedisContainer(),
+		},
+		Volumes: corev1.VolumeArray{
+			rds.createRedisVolume(pvc),
+		},
+	}
+}
+
+func (rds *RedisStandalone) createInitContainer() *corev1.ContainerArgs {
+	return &corev1.ContainerArgs{
+		Name:  pulumi.String("init-redis"),
+		Image: pulumi.String("busybox"),
+		Command: pulumi.StringArray{
+			pulumi.String("sh"),
+			pulumi.String("-c"),
+			pulumi.String("chown -R 1000 /data"),
+		},
+		VolumeMounts: corev1.VolumeMountArray{
+			&corev1.VolumeMountArgs{
+				Name: pulumi.String(
+					fmt.Sprintf("%s-data", rds.Config.Name),
+				),
+				MountPath: pulumi.String("/data"),
 			},
 		},
 	}
 }
 
-func (rds *RedisStandalone) createStorageResources() *StorageResourcesArgs {
-	return &StorageResourcesArgs{
-		Requests: pulumi.Map{
-			"storage": pulumi.String(rds.Config.Storage.Size),
+func (rds *RedisStandalone) createRedisContainer() *corev1.ContainerArgs {
+	return &corev1.ContainerArgs{
+		Name: pulumi.String(rds.Config.Name),
+		Image: pulumi.String(
+			fmt.Sprintf("%s:%s", rds.Config.Image.Name, rds.Config.Image.Tag),
+		),
+		Ports: corev1.ContainerPortArray{
+			&corev1.ContainerPortArgs{ContainerPort: pulumi.Int(6379)},
+		},
+		VolumeMounts: corev1.VolumeMountArray{
+			&corev1.VolumeMountArgs{
+				Name: pulumi.String(
+					fmt.Sprintf("%s-data", rds.Config.Name),
+				),
+				MountPath: pulumi.String("/data"),
+			},
 		},
 	}
 }
 
-func (rds *RedisStandalone) createKubernetesConfig() *redisv1beta2.RedisSpecKubernetesConfigArgs {
-	return &redisv1beta2.RedisSpecKubernetesConfigArgs{
-		Image: pulumi.String(
-			fmt.Sprintf("%s:%s", rds.Config.Image.Name, rds.Config.Image.Tag),
-		),
-		ImagePullPolicy: pulumi.String("IfNotPresent"),
+func (rds *RedisStandalone) createRedisVolume(
+	pvc *corev1.PersistentVolumeClaim,
+) *corev1.VolumeArgs {
+	return &corev1.VolumeArgs{
+		Name: pulumi.String(fmt.Sprintf("%s-data", rds.Config.Name)),
+		PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSourceArgs{
+			ClaimName: pvc.Metadata.Name().Elem(),
+		},
 	}
 }
 
-func (rds *RedisStandalone) createLivenessProbe() *redisv1beta2.RedisSpecLivenessProbeArgs {
-	return &redisv1beta2.RedisSpecLivenessProbeArgs{
-		FailureThreshold:    pulumi.Int(5),
-		InitialDelaySeconds: pulumi.Int(15),
-		PeriodSeconds:       pulumi.Int(15),
-		SuccessThreshold:    pulumi.Int(1),
-		TimeoutSeconds:      pulumi.Int(5),
+func (rds *RedisStandalone) createMetadata() *metav1.ObjectMetaArgs {
+	return &metav1.ObjectMetaArgs{
+		Name:      pulumi.String(rds.Config.Name),
+		Namespace: pulumi.String(rds.Config.Namespace),
+		Labels:    rds.createLabels(),
 	}
 }
 
-func (rds *RedisStandalone) createReadinessProbe() *redisv1beta2.RedisSpecReadinessProbeArgs {
-	return &redisv1beta2.RedisSpecReadinessProbeArgs{
-		FailureThreshold:    pulumi.Int(5),
-		InitialDelaySeconds: pulumi.Int(15),
-		PeriodSeconds:       pulumi.Int(15),
-		SuccessThreshold:    pulumi.Int(1),
-		TimeoutSeconds:      pulumi.Int(5),
+func (rds *RedisStandalone) createLabels() pulumi.StringMap {
+	return pulumi.StringMap{
+		"app": pulumi.String(rds.Config.Name),
 	}
 }
 
