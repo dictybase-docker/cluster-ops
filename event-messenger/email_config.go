cc0d30d70976bd04a54b1871c74025cc56dbeb91
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-29 19:12:16 -0500

    refactor(email_config.go): rename variables for better readability and consistency
    The variables graphqlConfig and eventMessengerEmail have been renamed to improve readability and consistency within the codebase. This change enhances the maintainability of the code by making it easier for developers to understand the purpose of these variables.

--- event-messenger/email_config.go
+++ event-messenger/email_config.go
@@ -6,26 +6,6 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
-type SecretConfig struct {
-  Name string
-  Key string
-}
-
-type ConfigMapEntry struct {
-  Name string
-  Key string
-}
-
-type ImageProperties struct {
-  Repository string
-  Tag string
-  PullPolicy string
-}
-
-type NatsProperties struct {
-  Subject string
-}
-
 type EventMessengerEmailConfig struct {
 	Namespace string
 	Replicas  int
@@ -42,10 +22,10 @@ type EventMessengerEmailConfig struct {
 
 func ReadEventMessengerEmailConfig(ctx *pulumi.Context) (*EventMessengerEmailConfig, error) {
 	conf := config.New(ctx, "event-messenger-email")
-	graphqlConfig := &EventMessengerEmailConfig{}
-	if err := conf.TryObject("properties", graphqlConfig); err != nil {
+	eventMessengerEmail := &EventMessengerEmailConfig{}
+	if err := conf.TryObject("properties", eventMessengerEmail); err != nil {
 		return nil, fmt.Errorf("failed to read event-messenger-email config: %w", err)
 	}
-	return graphqlConfig, nil
+	return eventMessengerEmail, nil
 }
 
