baa8e748091ec1638632e4a7532275cea602afd6
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-29 20:11:02 -0500

    feat(issue_container.go): add functions to generate environment variables and container configuration for EventMessengerIssue
    The functions added in this commit allow for the generation of environment variables and container configuration for the EventMessengerIssue struct. These functions help in creating a more modular and organized approach to setting up the necessary environment variables and container configuration for the EventMessengerIssue, improving code readability and maintainability.

---
+++ event-messenger/issue_container.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+  "fmt"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func (emi *EventMessengerIssue) ConfigMapEnvArgsArray() corev1.EnvVarArray {
+	envVars := []struct {
+		name      string
+		configMap ConfigMapEntry
+	}{
+		{"GITHUB_REPOSITORY", emi.Config.GithubRepo},
+		{"GITHUB_OWNER", emi.Config.GithubOwner},
+	}
+
+	var envVarArray corev1.EnvVarArray
+	for _, envVar := range envVars {
+		envVarArray = append(envVarArray, &corev1.EnvVarArgs{
+			Name: pulumi.String(envVar.name),
+			ValueFrom: &corev1.EnvVarSourceArgs{
+				ConfigMapKeyRef: &corev1.ConfigMapKeySelectorArgs{
+					Name: pulumi.String(envVar.configMap.Name),
+					Key:  pulumi.String(envVar.configMap.Key),
+				},
+			},
+		})
+	}
+	return envVarArray
+}
+
+func (emi *EventMessengerIssue) SecretEnvArgsArray() corev1.EnvVarArray {
+  return corev1.EnvVarArray{
+    &corev1.EnvVarArgs{
+			Name: pulumi.String("GITHUB_TOKEN"),
+			ValueFrom: &corev1.EnvVarSourceArgs{
+				ConfigMapKeyRef: &corev1.ConfigMapKeySelectorArgs{
+					Name: pulumi.String(emi.Config.GithubToken.Name),
+					Key:  pulumi.String(emi.Config.GithubToken.Key),
+				},
+			},
+    },
+  }
+}
+
+func (emi *EventMessengerIssue) ContainerEnvArgsArray() corev1.EnvVarArray {
+	var envVarArray corev1.EnvVarArray
+	envVarArray = append(envVarArray, emi.ConfigMapEnvArgsArray()...)
+	envVarArray = append(envVarArray, emi.SecretEnvArgsArray()...)
+	return envVarArray
+}
+
+func (emi *EventMessengerIssue) ContainerArgs() pulumi.StringArray {
+  args := []string{
+    "gh-issue",
+    "--log-level",
+    emi.Config.LogLevel,
+    "--subject",
+    emi.Config.Nats.Subject,
+    "--token",
+    "$(GITHUB_TOKEN)",
+    "--repository",
+    "$(GITHUB_REPOSITORY)",
+    "--owner",
+    "$(GITHUB_OWNER)",
+  }
+  return pulumi.ToStringArray(args)
+}
+
+func (emi *EventMessengerIssue) ContainerArray() corev1.ContainerArray {
+	config := emi.Config
+	return corev1.ContainerArray{
+		&corev1.ContainerArgs{
+			Name:  pulumi.String("event-messenger-issue"),
+			Image: pulumi.String(fmt.Sprintf("%s:%s", config.Image, config.Image.Tag)),
+      Args: emi.ContainerArgs(),
+			Env:   emi.ContainerEnvArgsArray(),
+		},
+	}
+}
+
