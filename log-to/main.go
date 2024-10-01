3e8d5240f25c9c37397bf64c4cfbde674e61a9a9
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-01 12:00:19 -0500

    feat(main.go): add support for reading configuration from pulumi config to enhance flexibility and reusability
    By adding the `ReadConfig` function, the application can now read configuration values from the pulumi config, allowing for greater flexibility and reusability. This change enables the application to dynamically retrieve configuration values at runtime, making it easier to manage and update configuration settings without modifying the code.

--- log-to/main.go
+++ log-to/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+  "fmt"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 type LogtoConfig struct {
@@ -37,4 +39,59 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
+}
+
+func Run(ctx *pulumi.Context) error {
+  config, err := ReadConfig(ctx)
+  if err != nil {
+    return err
+  }
+
+  lt := NewLogto(config)
+
+	if err := lt.Install(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewLogto(config *LogtoConfig) *Logto {
+  return &Logto{
+    Config: config,
+  }
+}
+
+func (lt *Logto) Install(ctx *pulumi.Context) error {
+	pvc, err := lt.CreatePersistentVolumeClaim(ctx)
+	if err != nil {
+		return err
+	}
+  
+  claimName := pvc.Metadata.Name().Elem()
+
+  deployment, err := lt.CreateDeployment(ctx, claimName)
+	if err != nil {
+		return err
+	}
+
+  _, err = lt.CreateService(ctx, deployment.Metadata.Name().Elem(), fmt.Sprintf("%s-api", lt.Config.Name), lt.Config.ApiPort)
+	if err != nil {
+		return err
+	}
+  _, err = lt.CreateService(ctx, deployment.Metadata.Name().Elem(), fmt.Sprintf("%s-admin", lt.Config.Name), lt.Config.AdminPort)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadConfig(ctx *pulumi.Context) (*LogtoConfig, error) {
+	conf := config.New(ctx, "log-to")
+	logtoConfig := &LogtoConfig{}
+	if err := conf.TryObject("properties", logtoConfig); err != nil {
+		return nil, fmt.Errorf("failed to read log-to config: %w", err)
+	}
+	return logtoConfig, nil
 }
