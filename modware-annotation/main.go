a71457dc4c879fbecb22f9beb318bde70fec6e74
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-10-01 18:14:22 -0500

    refactor(main.go): move Run function to backend package for better organization
    The Run function is now encapsulated within the backend package,
    improving code organization and separation of concerns. This change
    allows for better maintainability and reusability of the backend logic,
    while the main function in main.go is simplified to just call the
    backend.Run function.

--- modware-annotation/main.go
+++ modware-annotation/main.go
@@ -5,21 +5,6 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func Run(ctx *pulumi.Context) error {
-	backendConfig, err := backend.ReadConfig(ctx)
-	if err != nil {
-		return err
-	}
-
-	bck := backend.NewBackend(backendConfig)
-
-	if err := bck.Install(ctx); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func main() {
-	pulumi.Run(Run)
+	pulumi.Run(backend.Run)
 }
