90c2e2176df40b9127ce5cd3dd3809165f31f011
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-01 12:16:17 -0500

    feat(config.go): add LogtoConfig struct and ReadConfig function to read log-to configuration
    The LogtoConfig struct is added to define the configuration for the log-to application. The ReadConfig function is implemented to read the log-to configuration using the pulumi config package. The removal of the redundant types SecretKeyPair, ConfigMapPair, and ImageConfig from types.go simplifies the codebase by eliminating unused and duplicate definitions.

---
+++ log-to/config.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+  "fmt"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+type LogtoConfig struct {
+  Name string
+  Namespace string
+  Image ImageConfig
+  StorageClass string
+  DiskSize int
+  Database DatabaseProperties
+  Endpoint string
+  ApiPort int
+  AdminPort int
+}
+
+type DatabaseProperties struct {
+  Name string
+  User string
+  Host string
+  Port int
+  Password string
+}
+
+type SecretKeyPair struct {
+  Name string
+  Key string
+}
+
+type ConfigMapPair struct {
+  Name string
+  Key string
+}
+
+type ImageConfig struct {
+  Name string
+  Tag string
+}
+
+func ReadConfig(ctx *pulumi.Context) (*LogtoConfig, error) {
+	conf := config.New(ctx, "log-to")
+	logtoConfig := &LogtoConfig{}
+	if err := conf.TryObject("properties", logtoConfig); err != nil {
+		return nil, fmt.Errorf("failed to read log-to config: %w", err)
+	}
+	return logtoConfig, nil
+}
