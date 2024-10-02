429ec41af81daee20a763928dc80802086a0c6da
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-01 23:19:22 -0500

    feat(config): add GraphqlServerConfig struct and related sub-structs to manage server configuration
    The new GraphqlServerConfig struct is added to manage server configuration settings such as allowed origins, image details, log level, port, S3 bucket configuration, and secrets. This struct provides a centralized way to handle server configuration parameters, improving code organization and maintainability.

---
+++ graphql_server/config.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+type GraphqlServerConfig struct {
+	AllowedOrigins []string
+	ConfigMap      ConfigMap
+	Image          ImageConfig
+	LogLevel       string
+	Name           string
+	Namespace      string
+	Port           int
+	S3Bucket       S3BucketConfig
+	Secrets        SecretsConfig
+}
+
+type ConfigMap struct {
+	Name         string
+	EndpointKeys EndpointKeysConfig
+}
+
+type EndpointKeysConfig struct {
+	AuthEndpoint           string
+	OrganismEndpoint       string
+	PublicationApiEndpoint string
+	S3StorageEndpoint      string
+}
+
+type ImageConfig struct {
+	Name string
+	Tag  string
+}
+
+type S3BucketConfig struct {
+	Name string
+	Path string
+}
+
+type SecretsConfig struct {
+	Name      string
+	AuthKeys  AuthKeysConfig
+	MinioKeys MinioKeysConfig
+}
+
+type AuthKeysConfig struct {
+	AuthAppId     string
+	AuthAppSecret string
+	JwksURI       string
+	JwtAudience   string
+	JwtIssuer     string
+}
+
+type MinioKeysConfig struct {
+	MinioAccess string
+	MinioSecret string
+}
+
+func ReadConfig(ctx *pulumi.Context) (*GraphqlServerConfig, error) {
+	conf := config.New(ctx, "graphql_server")
+	graphqlConfig := &GraphqlServerConfig{}
+	if err := conf.TryObject("properties", graphqlConfig); err != nil {
+		return nil, fmt.Errorf("failed to read graphql config: %w", err)
+	}
+	return graphqlConfig, nil
+}
+
--- graphql_server/main.go
+++ graphql_server/main.go
@@ -1,50 +1,8 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
-
-type SecretKeyPair struct {
-	name string
-	key  string
-}
-
-type ConfigMapPair struct {
-	name string
-	key  string
-}
-
-type ImageConfig struct {
-	Name string
-	Tag  string
-}
-
-type GraphqlServerConfig struct {
-	Namespace              string
-	Name                   string
-	Image                  ImageConfig
-	LogLevel               string
-	Port                   int
-	SecretName             string
-	ConfigMapName          string
-	S3Bucket               string
-	S3BucketPath           string
-	AllowedOrigins         []string
-	AuthAppId              SecretKeyPair
-	AuthAppSecret          SecretKeyPair
-	AuthEndpoint           ConfigMapPair
-	JwksURI                SecretKeyPair
-	JwtAudience            SecretKeyPair
-	JwtIssuer              SecretKeyPair
-	MinioAccess            SecretKeyPair
-	MinioSecret            SecretKeyPair
-	OrganismEndpoint       ConfigMapPair
-	PublicationApiEndpoint ConfigMapPair
-	S3StorageEndpoint      ConfigMapPair
-}
 
 type GraphqlServer struct {
 	Config *GraphqlServerConfig
@@ -67,15 +25,6 @@ func Run(ctx *pulumi.Context) error {
 	}
 
 	return nil
-}
-
-func ReadConfig(ctx *pulumi.Context) (*GraphqlServerConfig, error) {
-	conf := config.New(ctx, "graphql_server")
-	graphqlConfig := &GraphqlServerConfig{}
-	if err := conf.TryObject("properties", graphqlConfig); err != nil {
-		return nil, fmt.Errorf("failed to read graphql config: %w", err)
-	}
-	return graphqlConfig, nil
 }
 
 func NewGraphqlServer(config *GraphqlServerConfig) *GraphqlServer {
