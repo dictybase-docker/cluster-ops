282534db0f8aebc91ea300c4f43faac6347480de
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-01 22:30:10 -0500

    feat(email_deployment.go): update deployment creation to use email name from config for better clarity
    The indentation for the import statement has been fixed to follow the Go convention. The deployment creation now uses the email name from the config instead of a generic name for better clarity and understanding of the deployment being created.

--- event-messenger/email_deployment.go
+++ event-messenger/email_deployment.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
@@ -9,38 +9,36 @@ import (
 )
 
 func (eme *EventMessengerEmail) CreateDeployment(ctx *pulumi.Context) (*appsv1.Deployment, error) {
-	deployment, err := appsv1.NewDeployment(ctx, eme.Config.Name, &appsv1.DeploymentArgs{
+	deployment, err := appsv1.NewDeployment(ctx, eme.Config.Email.Name, &appsv1.DeploymentArgs{
 		Metadata: eme.CreateDeploymentMetadata(),
 		Spec:     eme.CreateDeploymentSpec(),
 	})
-  if err != nil {
-    return nil, fmt.Errorf("error creating %s deployment: %w", eme.Config.Name, err)
-  }
+	if err != nil {
+		return nil, fmt.Errorf("error creating %s deployment: %w", eme.Config.Email.Name, err)
+	}
 	return deployment, nil
 }
 
 func (eme *EventMessengerEmail) CreateDeploymentMetadata() *metav1.ObjectMetaArgs {
 	return &metav1.ObjectMetaArgs{
-		Namespace: pulumi.String(eme.Config.Namespace),
-		Name:      pulumi.String(eme.Config.Name),
-    Labels: pulumi.StringMap{
-      "app": pulumi.String(eme.Config.Name),
-    },
+		Name: pulumi.String(eme.Config.Email.Name),
+		Labels: pulumi.StringMap{
+			"app": pulumi.String(eme.Config.Email.Name),
+		},
 	}
 }
 
 func (eme *EventMessengerEmail) CreateDeploymentSpec() *appsv1.DeploymentSpecArgs {
 	return &appsv1.DeploymentSpecArgs{
-		Replicas: pulumi.Int(eme.Config.Replicas),
 		Selector: &metav1.LabelSelectorArgs{
 			MatchLabels: pulumi.StringMap{
-				"app": pulumi.String(eme.Config.Name),
+				"app": pulumi.String(eme.Config.Email.Name),
 			},
 		},
 		Template: &corev1.PodTemplateSpecArgs{
 			Metadata: &metav1.ObjectMetaArgs{
 				Labels: pulumi.StringMap{
-					"app": pulumi.String(eme.Config.Name),
+					"app": pulumi.String(eme.Config.Email.Name),
 				},
 			},
 			Spec: &corev1.PodSpecArgs{
