cdb3fba9758989ad5fd4456649efe04ee8379c71
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-10-02 12:15:04 -0500

    refactor(event-messenger): replace Install method with direct CreateDeployment call for clarity and simplicity
    The Install method is removed in favor of directly calling
    CreateDeployment. This change simplifies the code by eliminating an
    unnecessary method, making the flow of the Run function clearer and
    reducing potential confusion.

--- event-messenger/main.go
+++ event-messenger/main.go
@@ -18,18 +18,16 @@ func main() {
 
 func Run(ctx *pulumi.Context) error {
 	emeConfig, err := ReadEventMessengerEmailConfig(ctx)
-
 	if err != nil {
 		return err
 	}
 
 	eventMessengerEmail := NewEventMessengerEmail(emeConfig)
 
-	if err := eventMessengerEmail.Install(ctx); err != nil {
+	if _, err := eventMessengerEmail.CreateDeployment(ctx); err != nil {
 		return err
 	}
 	emiConfig, err := ReadEventMessengerIssueConfig(ctx)
-
 	if err != nil {
 		return err
 	}
@@ -37,15 +35,6 @@ func Run(ctx *pulumi.Context) error {
 	eventMessengerIssue := NewEventMessengerIssue(emiConfig)
 
 	if err := eventMessengerIssue.Install(ctx); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (eme *EventMessengerEmail) Install(ctx *pulumi.Context) error {
-	_, err := eme.CreateDeployment(ctx)
-	if err != nil {
 		return err
 	}
 
