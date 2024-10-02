9fbcff9001320faedd3c9d8070408c2684d18e18
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-01 21:46:46 -0500

    refactor(email_config.go, issue_config.go): restructure email and issue configurations for better organization and readability
    The email and issue configurations have been restructured to improve organization and readability. The configurations now include separate structs for deployment and secrets, making it easier to manage and access specific information related to email and issue configurations. Additionally, the configuration keys have been updated to align with the new structure for consistency.

--- event-messenger/issue_config.go
+++ event-messenger/issue_config.go
@@ -1,28 +1,39 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 type EventMessengerIssueConfig struct {
-  Name string
-  Namespace string
-	Replicas  int
-  LogLevel string
-  Nats NatsProperties
-	Image     ImageConfig
-  GithubRepo ConfigMapPair
-  GithubOwner ConfigMapPair
-  GithubToken SecretKeyPair
+	LogLevel string
+	Nats     NatsProperties
+	Image    ImageConfig
+	Issue    IssueDeployment
+}
+
+type IssueDeployment struct {
+	Name    string
+	Secrets IssueSecrets
+}
+
+type IssueSecrets struct {
+	Name string
+	Keys IssueSecretKeys
+}
+
+type IssueSecretKeys struct {
+	Owner      string
+	Repository string
+	Token      string
 }
 
 func ReadEventMessengerIssueConfig(ctx *pulumi.Context) (*EventMessengerIssueConfig, error) {
-	conf := config.New(ctx, "event-messenger-issue")
+	conf := config.New(ctx, "event-messenger")
 	eventMessengerIssue := &EventMessengerIssueConfig{}
 	if err := conf.TryObject("properties", eventMessengerIssue); err != nil {
-		return nil, fmt.Errorf("failed to read event-messenger-issue config: %w", err)
+		return nil, fmt.Errorf("failed to read event-messenger config: %w", err)
 	}
 	return eventMessengerIssue, nil
 }
