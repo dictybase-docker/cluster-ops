d90a1022f8a18e6c251faf6af2136346c2737029
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-04 10:19:32 -0500

    feat(ingress.go): add functionality to create Ingress resources for Logto service
    The code introduces functions to create Ingress resources for the Logto service. The CreateIngress function creates a new Ingress resource with metadata and specifications. The IngressMetadata function sets the metadata for the Ingress, including annotations. The IngressSpec function defines the specifications for the Ingress, such as the IngressClassName, TLS configuration, and rules. The IngressTls and IngressRules functions handle TLS configuration and defining rules for the Ingress based on backend hosts. This functionality enhances the Logto service by enabling the creation of Ingress resources for routing traffic to the service.

---
+++ log-to/ingress.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+
+	networkingv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/networking/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func (lt *Logto) CreateIngress(ctx *pulumi.Context, service pulumi.Resource) (*networkingv1.Ingress, error) {
+	ingress, err := networkingv1.NewIngress(ctx, fmt.Sprintf("%s-ingress", lt.Config.Name), &networkingv1.IngressArgs{
+		Metadata: lt.IngressMetadata(),
+		Spec:     lt.IngressSpec(),
+	}, pulumi.DependsOn([]pulumi.Resource{service}))
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Logto Ingress resource: %w", err)
+	}
+
+	return ingress, nil
+}
+
+func (lt *Logto) IngressMetadata() metav1.ObjectMetaPtrInput {
+	return &metav1.ObjectMetaArgs{
+		Name:      pulumi.String(fmt.Sprintf("%s-ingress", lt.Config.Name)),
+		Namespace: pulumi.String(lt.Config.Namespace),
+		Annotations: pulumi.StringMap{
+			"cert-manager.io/issuer": pulumi.String(lt.Config.Ingress.Issuer),
+		},
+	}
+}
+
+func (lt *Logto) IngressSpec() networkingv1.IngressSpecPtrInput {
+	return &networkingv1.IngressSpecArgs{
+		IngressClassName: pulumi.String("nginx"),
+		Tls:              lt.IngressTls(),
+		Rules:            lt.IngressRules(),
+	}
+}
+
+func (lt *Logto) IngressTls() networkingv1.IngressTLSArrayInput {
+	return networkingv1.IngressTLSArray{
+		&networkingv1.IngressTLSArgs{
+			SecretName: pulumi.String(lt.Config.Ingress.TlsSecret),
+			Hosts:      pulumi.ToStringArray(lt.Config.Ingress.BackendHosts),
+		},
+	}
+}
+
+func (lt *Logto) IngressRules() networkingv1.IngressRuleArrayInput {
+	var rules networkingv1.IngressRuleArray
+	for _, h := range lt.Config.Ingress.BackendHosts {
+		rules = append(rules, &networkingv1.IngressRuleArgs{
+			Host: pulumi.String(h),
+			Http: &networkingv1.HTTPIngressRuleValueArgs{
+				Paths: networkingv1.HTTPIngressPathArray{
+					&networkingv1.HTTPIngressPathArgs{
+						Path:     pulumi.String("/"),
+						PathType: pulumi.String("Prefix"),
+						Backend: &networkingv1.IngressBackendArgs{
+							Service: &networkingv1.IngressServiceBackendArgs{
+								Name: pulumi.String(fmt.Sprintf("%s-api", lt.Config.Name)),
+								Port: &networkingv1.ServiceBackendPortArgs{
+									Number: pulumi.Int(lt.Config.APIPort),
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+	}
+	return rules
+}
