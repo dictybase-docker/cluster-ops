eefd2056376fca61e834912b867e9b2127de3829
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-07 15:35:46 -0500

    style(main.go): reformat code for better readability and consistency
    The code in main.go has been reformatted to improve readability and maintain consistency in the codebase. The changes include adjusting indentation, adding proper spacing, and aligning code blocks to enhance the overall code quality.

--- ingress-controller/main.go
+++ ingress-controller/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
+	nginxingress "github.com/pulumi/pulumi-kubernetes-ingress-nginx/sdk/go/kubernetes-ingress-nginx"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
-	nginxingress "github.com/pulumi/pulumi-kubernetes-ingress-nginx/sdk/go/kubernetes-ingress-nginx"
 )
 
 type Config struct {
@@ -16,20 +16,27 @@ func ReadConfig(ctx *pulumi.Context) (*Config, error) {
 	conf := config.New(ctx, "")
 	cfg := &Config{}
 	if err := conf.TryObject("properties", cfg); err != nil {
-		return nil, fmt.Errorf("failed to read ingress-controller config: %w", err)
+		return nil, fmt.Errorf(
+			"failed to read ingress-controller config: %w",
+			err,
+		)
 	}
 	return cfg, nil
 }
 
 func Run(ctx *pulumi.Context) error {
 	// Create an NGINX Ingress Controller
-  _, err := nginxingress.NewIngressController(ctx, "nginx-ingress", &nginxingress.IngressControllerArgs{
-		Controller: &nginxingress.ControllerArgs{
-			PublishService: &nginxingress.ControllerPublishServiceArgs{
-				Enabled: pulumi.Bool(true),
+	_, err := nginxingress.NewIngressController(
+		ctx,
+		"nginx-ingress",
+		&nginxingress.IngressControllerArgs{
+			Controller: &nginxingress.ControllerArgs{
+				PublishService: &nginxingress.ControllerPublishServiceArgs{
+					Enabled: pulumi.Bool(true),
+				},
 			},
 		},
-	})
+	)
 	if err != nil {
 		return err
 	}
