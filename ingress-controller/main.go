0917c4c84c39f1fdfe412e5db623a85996f27407
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-07 15:12:16 -0500

    feat(main.go): add NGINX Ingress Controller creation to manage incoming traffic
    The NGINX Ingress Controller is added to the main.go file to manage incoming traffic to the application. This controller is essential for routing external requests to the appropriate services within the Kubernetes cluster.

--- ingress-controller/main.go
+++ ingress-controller/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	nginxingress "github.com/pulumi/pulumi-kubernetes-ingress-nginx/sdk/go/kubernetes-ingress-nginx"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an NGINX Ingress Controller
+		_, err := nginxingress.NewIngressController(ctx, "nginx-ingress", &nginxingress.IngressControllerArgs{
+			Controller: &nginxingress.ControllerArgs{
+				PublishService: &nginxingress.ControllerPublishServiceArgs{
+					Enabled: pulumi.Bool(true),
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
 		return nil
 	})
 }
