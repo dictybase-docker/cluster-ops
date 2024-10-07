48b670a84bcc614420bf4127f900045dd026a2cc
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-10-07 11:30:55 -0500

    feat(custodian): refactor command handling into separate functions for better organization and readability
    The command handling for "extract-log" and "exclude-from-backup" is
    refactored into separate functions. This improves code organization,
    enhances readability, and makes it easier to maintain and extend the
    command functionality in the future. Additionally, a logger
    initialization function is introduced to streamline logger setup.

--- cmd/custodian/main.go
+++ cmd/custodian/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 
@@ -8,8 +9,82 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func initLogger() *slog.Logger {
+	return slog.New(slog.NewTextHandler(os.Stdout, nil))
+}
+
+func newCustodianConfig(
+	cliCtx *cli.Context,
+	logger *slog.Logger,
+) custodian.CustodianConfig {
+	return custodian.CustodianConfig{
+		KubeconfigPath: cliCtx.String("kubeconfig"),
+		Namespace:      cliCtx.String("namespace"),
+		Logger:         logger,
+	}
+}
+
+func extractLogCommand(logger *slog.Logger) *cli.Command {
+	return &cli.Command{
+		Name:  "extract-log",
+		Usage: "Search for Kubernetes jobs and extract logs",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "label",
+				Aliases:  []string{"l"},
+				Usage:    "Kubernetes label to search for",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:    "namespace",
+				Aliases: []string{"n"},
+				Usage:   "Kubernetes namespace to search in",
+				Value:   "dev",
+			},
+		},
+		Action: func(cliCtx *cli.Context) error {
+			config := newCustodianConfig(cliCtx, logger)
+			config.Label = cliCtx.String("label")
+			cus, err := custodian.NewCustodian(config)
+			if err != nil {
+				return cli.Exit(err.Error(), 2)
+			}
+			if err := cus.SearchAndExtractLogs(cliCtx); err != nil {
+				return cli.Exit(err.Error(), 2)
+			}
+			return nil
+		},
+	}
+}
+
+func excludeFromBackupCommand(logger *slog.Logger) *cli.Command {
+	return &cli.Command{
+		Name:  "exclude-from-backup",
+		Usage: "Add 'velero.io/exclude-from-backup=true' label",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "namespace",
+				Aliases: []string{"n"},
+				Usage:   "Kubernetes namespace to search in",
+				Value:   "dev",
+			},
+		},
+		Action: func(cliCtx *cli.Context) error {
+			config := newCustodianConfig(cliCtx, logger)
+			cus, err := custodian.NewCustodian(config)
+			if err != nil {
+				return cli.Exit(err.Error(), 2)
+			}
+			if err := cus.ExcludeFromBackup(); err != nil {
+				return cli.Exit(err.Error(), 2)
+			}
+			return nil
+		},
+	}
+}
+
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := initLogger()
 
 	app := &cli.App{
 		Name:  "custodian",
@@ -24,67 +99,14 @@ func main() {
 			},
 		},
 		Commands: []*cli.Command{
-			{
-				Name:  "extract-log",
-				Usage: "Search for Kubernetes jobs and extract logs",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:     "label",
-						Aliases:  []string{"l"},
-						Usage:    "Kubernetes label to search for",
-						Required: true,
-					},
-					&cli.StringFlag{
-						Name:    "namespace",
-						Aliases: []string{"n"},
-						Usage:   "Kubernetes namespace to search in",
-						Value:   "dev",
-					},
-				},
-				Action: func(cliCtx *cli.Context) error {
-					config := custodian.CustodianConfig{
-						KubeconfigPath: cliCtx.String("kubeconfig"),
-						Namespace:      cliCtx.String("namespace"),
-						Label:          cliCtx.String("label"),
-						Logger:         logger,
-					}
-					cus, err := custodian.NewCustodian(config)
-					if err != nil {
-						return err
-					}
-					return cus.SearchAndExtractLogs(cliCtx)
-				},
-			},
-			{
-				Name:  "exclude-from-backup",
-				Usage: "Add 'velero.io/exclude-from-backup=true' label to resources with label 'app.kubernetes.io/name=kube-arangodb'",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:    "namespace",
-						Aliases: []string{"n"},
-						Usage:   "Kubernetes namespace to search in",
-						Value:   "dev",
-					},
-				},
-				Action: func(cliCtx *cli.Context) error {
-					config := custodian.CustodianConfig{
-						KubeconfigPath: cliCtx.String("kubeconfig"),
-						Namespace:      cliCtx.String("namespace"),
-						Logger:         logger,
-					}
-					cus, err := custodian.NewCustodian(config)
-					if err != nil {
-						return err
-					}
-					return cus.ExcludeFromBackup()
-				},
-			},
+			extractLogCommand(logger),
+			excludeFromBackupCommand(logger),
 		},
 	}
 
 	err := app.Run(os.Args)
 	if err != nil {
-		logger.Error("Application error", "error", err)
+		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
 }
