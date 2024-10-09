e8966bb161b79fff69b8a3cac1a089b0905fbab7
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-10-09 03:56:56 -0500

    feat(config.go, container.go, deployment.go, main.go): add DatabaseSecret field to LogtoConfig and refactor related functions to use it
    The LogtoConfig struct now includes a DatabaseSecret field to store the
    name of the secret containing database credentials. Functions that
    create containers and deployments are updated to utilize this secret,
    enhancing security by avoiding hardcoded credentials.

--- log-to/deployment.go
+++ log-to/deployment.go
@@ -12,16 +12,16 @@ import (
 func (lt *Logto) CreateDeployment(
 	ctx *pulumi.Context,
 	claimName pulumi.StringInput,
+	dbSecretName string,
 ) (*appsv1.Deployment, error) {
 	deployment, err := appsv1.NewDeployment(
 		ctx,
 		lt.Config.Name,
 		&appsv1.DeploymentArgs{
 			Metadata: lt.CreateDeploymentMetadata(),
-			Spec:     lt.CreateDeploymentSpec(claimName),
+			Spec:     lt.CreateDeploymentSpec(claimName, dbSecretName),
 		},
 	)
-
 	if err != nil {
 		return nil, fmt.Errorf(
 			"error creating %s deployment: %w",
@@ -42,6 +42,7 @@ func (lt *Logto) CreateDeploymentMetadata() *metav1.ObjectMetaArgs {
 
 func (lt *Logto) CreateDeploymentSpec(
 	claimName pulumi.StringInput,
+	dbSecretName string,
 ) *appsv1.DeploymentSpecArgs {
 	return &appsv1.DeploymentSpecArgs{
 		Selector: &metav1.LabelSelectorArgs{
@@ -56,7 +57,7 @@ func (lt *Logto) CreateDeploymentSpec(
 				},
 			},
 			Spec: &corev1.PodSpecArgs{
-				Containers: lt.ContainerArray(),
+				Containers: lt.ContainerArray(dbSecretName),
 				Volumes: corev1.VolumeArray{
 					&corev1.VolumeArgs{
 						Name: pulumi.String(
@@ -66,8 +67,78 @@ func (lt *Logto) CreateDeploymentSpec(
 							ClaimName: claimName,
 						},
 					},
+					&corev1.VolumeArgs{
+						Name: pulumi.String("db-secret"),
+						Secret: &corev1.SecretVolumeSourceArgs{
+							SecretName: dbSecretName,
+						},
+					},
 				},
 			},
 		},
 	}
+}
+
+func (lt *Logto) CreatePersistentVolumeClaim(
+	ctx *pulumi.Context,
+) (*corev1.PersistentVolumeClaim, error) {
+	pvc, err := corev1.NewPersistentVolumeClaim(
+		ctx,
+		fmt.Sprintf("%s-pvc", lt.Config.Name),
+		&corev1.PersistentVolumeClaimArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:      pulumi.String(fmt.Sprintf("%s-pvc", lt.Config.Name)),
+				Namespace: pulumi.String(lt.Config.Namespace),
+			},
+			Spec: &corev1.PersistentVolumeClaimSpecArgs{
+				AccessModes: pulumi.StringArray{pulumi.String("ReadWriteOnce")},
+				Resources: &corev1.ResourceRequirementsArgs{
+					Requests: pulumi.StringMap{
+						"storage": pulumi.String(lt.Config.DiskSize),
+					},
+				},
+				StorageClassName: pulumi.String(lt.Config.StorageClass),
+			},
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"error creating persistent volume claim: %w",
+			err,
+		)
+	}
+	return pvc, nil
+}
+
+func (lt *Logto) CreateService(
+	ctx *pulumi.Context,
+	appName pulumi.StringInput,
+	serviceName string,
+	port int,
+) (*corev1.Service, error) {
+	service, err := corev1.NewService(ctx, serviceName, &corev1.ServiceArgs{
+		Metadata: &metav1.ObjectMetaArgs{
+			Name:      pulumi.String(serviceName),
+			Namespace: pulumi.String(lt.Config.Namespace),
+		},
+		Spec: &corev1.ServiceSpecArgs{
+			Selector: pulumi.StringMap{
+				"app": appName,
+			},
+			Ports: corev1.ServicePortArray{
+				&corev1.ServicePortArgs{
+					Port:       pulumi.Int(port),
+					TargetPort: pulumi.Int(port),
+				},
+			},
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf(
+			"error creating service %s: %w",
+			serviceName,
+			err,
+		)
+	}
+	return service, nil
 }
