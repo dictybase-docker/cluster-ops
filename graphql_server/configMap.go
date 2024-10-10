043b35e41700ba35d97a97151010e614fb44a018
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-10 17:28:57 -0500

    fix: reformat functions for better readability
    The changes were made to reformat the code in multiple files to improve readability and maintainability. By restructuring the code, it becomes easier for developers to understand and work with the codebase efficiently.

--- graphql_server/configMap.go
+++ graphql_server/configMap.go
@@ -8,37 +8,70 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func (gs *GraphqlServer) CreateConfigMap (ctx *pulumi.Context) (*corev1.ConfigMap, error) {
-  config := gs.Config
-  configMapData := pulumi.StringMap{
-    config.ConfigMap.EndpointKeys.AuthEndpoint: pulumi.String(config.ConfigMap.EndpointValues.AuthEndpoint),
-    config.ConfigMap.EndpointKeys.OrganismEndpoint: pulumi.String(config.ConfigMap.EndpointValues.OrganismEndpoint),
-    config.ConfigMap.EndpointKeys.PublicationAPIEndpoint: pulumi.String(config.ConfigMap.EndpointValues.PublicationAPIEndpoint),
-    config.ConfigMap.EndpointKeys.S3StorageEndpoint: pulumi.String(config.ConfigMap.EndpointValues.S3StorageEndpoint),
-    config.ConfigMap.GRPCKeys.StockHost: pulumi.String(config.ConfigMap.GRPCValues.StockHost),
-    config.ConfigMap.GRPCKeys.StockPort: pulumi.String(config.ConfigMap.GRPCValues.StockPort),
-    config.ConfigMap.GRPCKeys.OrderHost: pulumi.String(config.ConfigMap.GRPCValues.OrderHost),
-    config.ConfigMap.GRPCKeys.OrderPort: pulumi.String(config.ConfigMap.GRPCValues.OrderPort),
-    config.ConfigMap.GRPCKeys.AnnotationHost: pulumi.String(config.ConfigMap.GRPCValues.AnnotationHost),
-    config.ConfigMap.GRPCKeys.AnnotationPort: pulumi.String(config.ConfigMap.GRPCValues.AnnotationPort),
-    config.ConfigMap.GRPCKeys.ContentHost: pulumi.String(config.ConfigMap.GRPCValues.ContentHost),
-    config.ConfigMap.GRPCKeys.ContentPort: pulumi.String(config.ConfigMap.GRPCValues.ContentPort),
-    config.ConfigMap.GRPCKeys.RedisHost: pulumi.String(config.ConfigMap.GRPCValues.RedisHost),
-    config.ConfigMap.GRPCKeys.RedisPort: pulumi.String(config.ConfigMap.GRPCValues.RedisPort),
-  }
+func (gs *GraphqlServer) CreateConfigMap(
+	ctx *pulumi.Context,
+) (*corev1.ConfigMap, error) {
+	config := gs.Config
+	configMapData := pulumi.StringMap{
+		config.ConfigMap.EndpointKeys.AuthEndpoint: pulumi.String(
+			config.ConfigMap.EndpointValues.AuthEndpoint,
+		),
+		config.ConfigMap.EndpointKeys.OrganismEndpoint: pulumi.String(
+			config.ConfigMap.EndpointValues.OrganismEndpoint,
+		),
+		config.ConfigMap.EndpointKeys.PublicationAPIEndpoint: pulumi.String(
+			config.ConfigMap.EndpointValues.PublicationAPIEndpoint,
+		),
+		config.ConfigMap.EndpointKeys.S3StorageEndpoint: pulumi.String(
+			config.ConfigMap.EndpointValues.S3StorageEndpoint,
+		),
+		config.ConfigMap.GRPCKeys.StockHost: pulumi.String(
+			config.ConfigMap.GRPCValues.StockHost,
+		),
+		config.ConfigMap.GRPCKeys.StockPort: pulumi.String(
+			config.ConfigMap.GRPCValues.StockPort,
+		),
+		config.ConfigMap.GRPCKeys.OrderHost: pulumi.String(
+			config.ConfigMap.GRPCValues.OrderHost,
+		),
+		config.ConfigMap.GRPCKeys.OrderPort: pulumi.String(
+			config.ConfigMap.GRPCValues.OrderPort,
+		),
+		config.ConfigMap.GRPCKeys.AnnotationHost: pulumi.String(
+			config.ConfigMap.GRPCValues.AnnotationHost,
+		),
+		config.ConfigMap.GRPCKeys.AnnotationPort: pulumi.String(
+			config.ConfigMap.GRPCValues.AnnotationPort,
+		),
+		config.ConfigMap.GRPCKeys.ContentHost: pulumi.String(
+			config.ConfigMap.GRPCValues.ContentHost,
+		),
+		config.ConfigMap.GRPCKeys.ContentPort: pulumi.String(
+			config.ConfigMap.GRPCValues.ContentPort,
+		),
+		config.ConfigMap.GRPCKeys.RedisHost: pulumi.String(
+			config.ConfigMap.GRPCValues.RedisHost,
+		),
+		config.ConfigMap.GRPCKeys.RedisPort: pulumi.String(
+			config.ConfigMap.GRPCValues.RedisPort,
+		),
+	}
 
-  configMap, err := corev1.NewConfigMap(ctx, config.ConfigMap.Name, &corev1.ConfigMapArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(config.ConfigMap.Name),
-			Namespace: pulumi.String(config.Namespace),
+	configMap, err := corev1.NewConfigMap(
+		ctx,
+		config.ConfigMap.Name,
+		&corev1.ConfigMapArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:      pulumi.String(config.ConfigMap.Name),
+				Namespace: pulumi.String(config.Namespace),
+			},
+			Data: configMapData,
 		},
-    Data: configMapData,
-  })
+	)
 
-  if err != nil {
+	if err != nil {
 		return nil, fmt.Errorf("error creating configMap: %w", err)
-  }
+	}
 
-  return configMap, nil
+	return configMap, nil
 }
-
--- graphql_server/secret.go
+++ graphql_server/secret.go
@@ -8,30 +8,49 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func (gs *GraphqlServer) CreateSecret (ctx *pulumi.Context) (*corev1.Secret, error) {
-  config := gs.Config
-  secretsData := pulumi.StringMap{
-    config.Secrets.AuthKeys.AuthAppId: pulumi.String(config.Secrets.AuthValues.AuthAppId),
-    config.Secrets.AuthKeys.AuthAppSecret: pulumi.String(config.Secrets.AuthValues.AuthAppSecret),
-    config.Secrets.AuthKeys.JwksURI: pulumi.String(config.Secrets.AuthValues.JwksURI),
-    config.Secrets.AuthKeys.JwtAudience: pulumi.String(config.Secrets.AuthValues.JwtAudience),
-    config.Secrets.AuthKeys.JwtIssuer: pulumi.String(config.Secrets.AuthValues.JwtIssuer),
-    config.Secrets.MinioKeys.MinioAccess: pulumi.String(config.Secrets.MinioValues.MinioAccess),
-    config.Secrets.MinioKeys.MinioSecret: pulumi.String(config.Secrets.MinioValues.MinioSecret),
-  }
+func (gs *GraphqlServer) CreateSecret(
+	ctx *pulumi.Context,
+) (*corev1.Secret, error) {
+	config := gs.Config
+	secretsData := pulumi.StringMap{
+		config.Secrets.AuthKeys.AuthAppId: pulumi.String(
+			config.Secrets.AuthValues.AuthAppId,
+		),
+		config.Secrets.AuthKeys.AuthAppSecret: pulumi.String(
+			config.Secrets.AuthValues.AuthAppSecret,
+		),
+		config.Secrets.AuthKeys.JwksURI: pulumi.String(
+			config.Secrets.AuthValues.JwksURI,
+		),
+		config.Secrets.AuthKeys.JwtAudience: pulumi.String(
+			config.Secrets.AuthValues.JwtAudience,
+		),
+		config.Secrets.AuthKeys.JwtIssuer: pulumi.String(
+			config.Secrets.AuthValues.JwtIssuer,
+		),
+		config.Secrets.MinioKeys.MinioAccess: pulumi.String(
+			config.Secrets.MinioValues.MinioAccess,
+		),
+		config.Secrets.MinioKeys.MinioSecret: pulumi.String(
+			config.Secrets.MinioValues.MinioSecret,
+		),
+	}
 
-  configMap, err := corev1.NewSecret(ctx, config.Secrets.Name, &corev1.SecretArgs{
-		Metadata: &metav1.ObjectMetaArgs{
-			Name:      pulumi.String(config.Secrets.Name),
-			Namespace: pulumi.String(config.Namespace),
+	configMap, err := corev1.NewSecret(
+		ctx,
+		config.Secrets.Name,
+		&corev1.SecretArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:      pulumi.String(config.Secrets.Name),
+				Namespace: pulumi.String(config.Namespace),
+			},
+			StringData: secretsData,
 		},
-    StringData: secretsData,
-  })
+	)
 
-  if err != nil {
+	if err != nil {
 		return nil, fmt.Errorf("error creating secret: %w", err)
-  }
+	}
 
-  return configMap, nil
+	return configMap, nil
 }
-
