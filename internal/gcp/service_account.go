e961a1a38ab8b48fd4a0f1c25209fb3d3eb32502
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-22 16:46:44 -0600

    feat(service_account.go): add logging statements to improve visibility during service account creation process
    The logging statements using slog.Info() have been added to provide more visibility and insight into the service account creation process. This change aims to enhance monitoring and debugging capabilities during the creation of service accounts within the application.
    
    issue #136

--- internal/gcp/service_account.go
+++ internal/gcp/service_account.go
@@ -3,18 +3,20 @@ package gcp
 import (
 	"context"
 	"fmt"
+  "log/slog"
 
 	"github.com/urfave/cli/v2"
 	iam "google.golang.org/api/iam/v1"
 )
 
 // createServiceAccount creates a service account.
-func CreateServiceAccount(cliContext *cli.Context) (*iam.ServiceAccount, error) {
+func CreateServiceAccount(cliContext *cli.Context) error {
 	ctx := context.Background()
+  slog.Info("Creating Service Account...")
   
 	service, err := iam.NewService(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("iam.NewService: %w", err)
+		return fmt.Errorf("iam.NewService: %w", err)
 	}
 
 	request := &iam.CreateServiceAccountRequest{
@@ -23,11 +25,13 @@ func CreateServiceAccount(cliContext *cli.Context) (*iam.ServiceAccount, error) 
 			DisplayName: cliContext.String("sa-description"),
 		},
 	}
-  projectName := fmt.Sprintf("projects/%s", cliContext.String("project"))
 
-	account, err := service.Projects.ServiceAccounts.Create(projectName, request).Do()
+  projectName := fmt.Sprintf("projects/%s", cliContext.String("project"))
+	_, err = service.Projects.ServiceAccounts.Create(projectName, request).Do()
 	if err != nil {
-		return nil, fmt.Errorf("Projects.ServiceAccounts.Create: %w", err)
+		return fmt.Errorf("Projects.ServiceAccounts.Create: %w", err)
 	}
-	return account, nil
+
+  slog.Info("Service Account successfully created")
+	return nil
 }
