108a81107e94f5b905d2900fa6cf71069fb828e3
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-30 09:40:55 -0600

    feat(gcp): refactor CreateServiceAccount function to improve readability and
    modularity
    The CreateServiceAccount function in the service_account.go file has been
    refactored to improve readability and modularity. The function has been split
    into smaller functions such as createServiceAccount, assignRoles, and
    createServiceAccountKey to handle specific tasks related to creating a service
    account. This refactoring makes the code easier to understand and maintain.
    issue #136

--- internal/gcp/service_account.go
+++ internal/gcp/service_account.go
@@ -2,61 +2,103 @@ package gcp
 import (
 	"context"
 	"fmt"
-  "log/slog"
+    "log/slog"
 
 	"github.com/urfave/cli/v2"
 	iam "google.golang.org/api/iam/v1"
 )
 
+type IAMClient struct {
+    service *iam.Service
+}
+
 // createServiceAccount creates a service account.
 func CreateServiceAccount(cliContext *cli.Context) error {
-	ctx := context.Background()
-  saName := cliContext.String("name")
-  saDisplayName := cliContext.String("display-name")
-  saDescription := cliContext.String("description")
-  projectName := cliContext.String("project")
+    saName := cliContext.String("name")
+    saDisplayName := cliContext.String("display-name")
+    saDescription := cliContext.String("description")
+    projectName := cliContext.String("project")
+    rolesFilePath := cliContext.String("roles-path")
+    credentialOutputPath := cliContext.String("output-path")
 
-  slog.Info(fmt.Sprintf("Creating service account %s in %s...", saName, projectName))
-  
-	service, err := iam.NewService(ctx)
-	if err != nil {
-    slog.Error("Error creating client", "error", err)
-		return fmt.Errorf("iam.NewService: %w", err)
-	}
+    // Create Service
+    ctx := context.Background()
+    newService, err := iam.NewService(ctx)
+    if err != nil {
+        slog.Error("Error creating client", "error", err)
+        return err
+    }
 
-	request := &iam.CreateServiceAccountRequest{
-		AccountId: saName,
-		ServiceAccount: &iam.ServiceAccount{
-			DisplayName: saDisplayName,
-      Description: saDescription,
-		},
-	}
+    // Create Service Account
+    slog.Info(fmt.Sprintf("Creating service account %s in %s...", saName, projectName))
+    sa, err := createServiceAccount(newService, saName, projectName, saDisplayName, saDescription)
+    if err != nil {
+        slog.Error("Error creating service account", "error", err)
+        return err
+    }
 
-	_, err = service.Projects.ServiceAccounts.Create(fmt.Sprintf("projects/%s", projectName), request).Do()
-	if err != nil {
-    slog.Error("Error creating service account", "error", err)
-		return fmt.Errorf("Projects.ServiceAccounts.Create: %w", err)
-	}
+    // Assign Roles
+    err = assignRoles(newService, sa.Email, projectName, rolesFilePath)
+    if err != nil {
+        slog.Error("Error assigning roles", "error", err)
+        return err
+    }
 
-  slog.Info("Service Account successfully created")
-	return nil
+    // Output credentials
+    err = createServiceAccountKey(newService, sa.Email, projectName, credentialOutputPath)
+    if err != nil {
+        slog.Error("Error creating service account key", "error", err)
+        return err
+    }
+
+    return nil
 }
 
 func VerifyServiceAccount(cliContext *cli.Context) error {
-	ctx := context.Background()
-  saName := cliContext.String("name")
-  projectName := fmt.Sprintf("projects/%s", cliContext.String("project"))
-   
-	service, err := iam.NewService(ctx)
-	if err != nil {
-    slog.Error("Error creating client", "error", err)
-		return fmt.Errorf("iam.NewService: %w", err)
-	}
-  resourceName := fmt.Sprintf("projects/%s/serviceAccounts/%s", projectName, saName)
-	_, err = service.Projects.ServiceAccounts.Get(resourceName).Do()
-	if err != nil {
-    slog.Error("Could not find requested service account", "error", err)
-		return fmt.Errorf("Projects.ServiceAccounts.Get: %w", err)
-	}
-  return nil
+    ctx := context.Background()
+    saName := cliContext.String("name")
+    projectName := fmt.Sprintf("projects/%s", cliContext.String("project"))
+
+    service, err := iam.NewService(ctx)
+    if err != nil {
+        slog.Error("Error creating client", "error", err)
+        return fmt.Errorf("iam.NewService: %w", err)
+    }
+    resourceName := fmt.Sprintf("projects/%s/serviceAccounts/%s", projectName, saName)
+    _, err = service.Projects.ServiceAccounts.Get(resourceName).Do()
+    if err != nil {
+        slog.Error("Could not find requested service account", "error", err)
+        return fmt.Errorf("Projects.ServiceAccounts.Get: %w", err)
+    }
+    return nil
+}
+
+func createServiceAccount(service *iam.Service, name string, project string, displayName string, description string) (*iam.ServiceAccount, error) {
+    request := &iam.CreateServiceAccountRequest{
+        AccountId: name,
+        ServiceAccount: &iam.ServiceAccount{
+            DisplayName: displayName,
+            Description: description,
+        },
+    }
+
+    serviceAccount, err := service.Projects.ServiceAccounts.Create(fmt.Sprintf("projects/%s", project), request).Do()
+    if err != nil {
+        return nil, fmt.Errorf("Projects.ServiceAccounts.Create: %w", err)
+    }
+
+    slog.Info("Service Account successfully created")
+    return serviceAccount, nil
+}
+
+func assignRoles(service *iam.Service, serviceAccountEmail, projectName, rolesFilePath string) error {
+    // Code to assign roles to the service account
+    // You can read the roles from the rolesFilePath and use the iam.Service.Projects.ServiceAccounts.SetIamPolicy method
+    return nil
+}
+
+func createServiceAccountKey(service *iam.Service, serviceAccountEmail, projectName, credentialOutputPath string) error {
+    // Code to create a service account key and write it to the credentialOutputPath
+    // You can use the iam.Service.Projects.ServiceAccounts.Keys.Create method
+    return nil
 }
