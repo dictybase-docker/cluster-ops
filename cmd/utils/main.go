049e42d42614240f3f6db9e2840462aa429172d4
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-06 11:29:51 -0600

    feat(cmd/utils): add a command line utility for cluster operations
    This commit introduces a new command-line utility, `cluster-ops-utils`,
    designed to provide miscellaneous tools for the cluster-ops project.
    Currently, it includes a `set-env-var` command that allows users to
    easily set environmental variables in their `.envrc` file and load them.
    This utility aims to streamline common cluster operation tasks and
    improve the overall development experience. It uses the `urfave/cli/v2`
    package for command-line argument parsing and includes basic error
    handling with logging.

---
+++ cmd/utils/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/dictybase-docker/cluster-ops/internal/util"
+	"github.com/urfave/cli/v2"
+)
+
+var logger *slog.Logger
+
+func main() {
+	app := &cli.App{
+		Name:     "cluster-ops-utils",
+		Usage:    "miscellaneous tools for the cluster-ops project",
+		Commands: getCommands(),
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		logger.Error("Error running application", slog.Any("error", err))
+		os.Exit(1)
+	}
+}
+
+func getCommands() []*cli.Command {
+	return []*cli.Command{
+		setEnvironmentalVariableCommand(),
+	}
+}
+
+func setEnvironmentalVariableCommand() *cli.Command {
+	return &cli.Command{
+		Name:   "set-env-var",
+		Usage:  "set an enviromental variable in your .envrc file and load it",
+		Action: util.SetEnvironmentalVariable,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "name",
+				Aliases:  []string{"n"},
+				Usage:    "Name of Enviromental Variable",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "value",
+				Aliases:  []string{"v"},
+				Usage:    "Google Cloud project ID",
+				Required: true,
+			},
+		},
+	}
+}
